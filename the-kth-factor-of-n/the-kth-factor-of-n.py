8eb072766e5bf2c8871c766423f25a5dda5d8fe4
Wonhee Jang <Qtsky89@gmail.com>
2023-04-22 13:29:03 +0900

    Time: 32 ms (71.98%) | Memory: 13.8 MB (94.94%) - LeetSync

--- the-kth-factor-of-n/the-kth-factor-of-n.py
+++ the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -1,7 +1,7 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         factor_count = 0
-        for i in range(1, 1000+1):
+        for i in range(1, n+1):
             if n % i == 0:
                 factor_count += 1
 
