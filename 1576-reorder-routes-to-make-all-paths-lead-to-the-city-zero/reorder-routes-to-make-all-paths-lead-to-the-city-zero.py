a49477bfa8f1c60a0a87734e319900ded69cb927
Wonhee Jang <Qtsky89@gmail.com>
2024-01-27 21:30:36 +0900

    Time: 922 ms (85.82%) | Memory: 47 MB (64.77%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        edges = {x: [] for x in range(n)}
+        for x, y in connections:
+            edges[x].append(y)
+
+        neighbors = {city: [] for city in range(n)}
+        for x, y in connections:
+            neighbors[x].append(y)
+            neighbors[y].append(x)
+
+        visited = set()
+        count = 0
+
+        q = deque([0])
+        while q:
+            # 0
+            item = q.popleft()
+            visited.add(item)
+
+            # neighbor = 1, 4 ...
+
+            for neighbor in neighbors[item]:
+                if neighbor not in visited:
+
+                    if neighbor in edges and item in edges[neighbor]:
+                        pass
+                    else:
+                        count += 1
+                    q.append(neighbor)
+        return count
+
+        
+        
