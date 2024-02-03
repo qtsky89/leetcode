503642af2f3efd85b5e7a8a62e2a61ff382f3f42
Wonhee Jang <Qtsky89@gmail.com>
2024-02-03 20:22:56 +0900

    Time: 59 ms (61.60%) | Memory: 19.8 MB (99.02%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -5,20 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-    def _next_min_val(self, node: TreeNode) -> int:
-        p = node
-        while p.left:
-            p = p.left
-        return p.val
+    def _maxvalue(self, root: TreeNode) -> int:
+        if root and root.right:
+            return self._maxvalue(root.right)
+        else:
+            return root.val
     
-    def _previous_max_val(self, node: TreeNode) -> int:
-        p = node
-        while p.right:
-            p = p.right
-        return p.val
-
+    def _minvalue(self, root: TreeNode) -> int:
+        if root and root.left:
+            return self._minvalue(root.left)
+        else:
+            return root.val
+        
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        '''
+            5
+        3      6
+    2     4        7
+
+        case1. if that node has no child, just delete it!
+        case2. if that node has only left child, swap left child's max value to that node
+        case3. if that node has right child, swap right child's min value to that node
+        case4. if that node has both childs, do that case2 or case3.
+        '''
+
         if not root:
             return None
         
@@ -26,16 +36,16 @@ class Solution:
             root.left = self.deleteNode(root.left, key)
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
-        else: # key = root.val
-            # leaf
+        else:
+            # case1. if that node has no child, just delete it!
             if not root.left and not root.right:
-                root = None
+                return None
+            # case2. if that node has only left child, swap left child's max value to that node
             elif root.left:
-                root.val = self._previous_max_val(root.left)
+                root.val = self._maxvalue(root.left)
                 root.left = self.deleteNode(root.left, root.val)
             else:
-                root.val = self._next_min_val(root.right)
+                root.val = self._minvalue(root.right)
                 root.right = self.deleteNode(root.right, root.val)
-        return root
 
-        
+        return root
