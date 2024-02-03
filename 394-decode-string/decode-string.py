6939535c8a53b886419364129d02be3c1052f63f
Wonhee Jang <Qtsky89@gmail.com>
2024-02-03 13:38:26 +0900

    Time: 37 ms (55.23%) | Memory: 16.6 MB (55.14%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,31 +1,47 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         '''
-        k[encoded_string]
 
+        [ ]
+        1. 
+        stack = [2 bc    ]
+        ret = [a a a bc bc]
 
-        2[abc]3[cd]ef
+        2. 
+        s = 3[a2[c]]
+
+        char, [ push to stack
+        ] pop out stack
+
+        stack = [acc acc acc]
+        return stack
         '''
-        stack = []
-        i = 0
-        while i <= len(s)-1:            
+
+        stack: list[str] = []
+
+        i = 0 
+        # 3 [a 2 [c]]
+        while i <= len(s)-1:
             if s[i].isdigit():
-                tmp = []
+                tmp = ''
+
                 while s[i].isdigit():
-                    tmp.append(s[i])
-                    i+=1
-                stack.append(int(''.join(tmp)))
-                continue
-            elif stack and s[i] in 'abcdefghijklmnopqrstuvwxyz[':
-                stack.append(s[i])
-            elif stack and s[i] == ']':
+                    tmp += s[i]
+                    i += 1
+                stack.append(tmp)
+            elif s[i] == ']':
+                # c
                 tmp = []
                 while stack and stack[-1] != '[':
                     tmp.append(stack.pop())
                 stack.pop()
-                stack.append(''.join(tmp[::-1] * int(stack.pop()) ))
+                # 3
+                count = stack.pop()
+                stack.append(''.join(tmp[::-1] * int(count) ))
+                i+=1 
             else:
                 stack.append(s[i])
-            i+=1
+                i+=1
         return ''.join(stack)
-
+                
+                
