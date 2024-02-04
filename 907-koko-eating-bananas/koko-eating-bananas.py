081306fdc5534b9e92f1a4ec406c47818d005b26
Wonhee Jang <Qtsky89@gmail.com>
2024-02-04 22:39:59 +0900

    Time: 261 ms (84.79%) | Memory: 18.1 MB (69.75%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,40 +1,34 @@
-import math
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         '''
-        https://www.youtube.com/watch?v=U2SozAs9RzA 
+        piles = [3, 6, 7, 11] h = 8
+                 
+        k = 1   3 + 6 + 7 + 11 = 27 hours, koko will be arrested
+        k = 2   2 + 3 + 4 + 6  = 15 hours,  koko will be arrested
+        k = 3   1 + 2 + 3 + 4 = 12 hours, koko will be arrested
+        k = 4   1 + 2 + 2 + 3 = 8 hours, koko will not be arrested ! yey!
+        k = larget piles   1 + 1 + 1 + 1 = 4 hours
 
-        piles = [3, 6, 7, 11]
-                 ^
-                 1  2   3   4 => 10 hours
-
-        minimum number of k => 3
-        h = 8
-
-        k=1 make bigger
-
-        k is in between 1 to max of pile
+        
+        1. every interation we cacluate the hours
+        2. hours == h return k // TODO. add some edge case
+        3. hours > h k = l + 1
+        4. hours < h k = r-1
         '''
 
         l, r = 1, max(piles)
-        k = 0
+
+        k = 1
         while l <= r:
             m = (l + r) // 2
 
-            eating_hour = 0
+            hours = 0
             for pile in piles:
-                eating_hour += math.ceil(pile / m)
-
-            if eating_hour <= h:
+                hours += math.ceil(pile / m)
+            
+            if hours <= h:
                 k = m
-                r = m-1
+                r = m - 1
             else:
-                l = m+1
+                l = m + 1
         return k
-
-            
-
-            
-        
-            
