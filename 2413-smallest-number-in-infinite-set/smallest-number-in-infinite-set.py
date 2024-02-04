157bc80a192d3375adfa9ed4473f545cd61deb66
Wonhee Jang <Qtsky89@gmail.com>
2024-02-04 16:55:10 +0900

    Time: 110 ms (45.27%) | Memory: 17.3 MB (57.56%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.py
@@ -1,24 +1,34 @@
-from heapq import heapify, heappop, heappush
+'''
+    s = SmallestInfiniteSet()
+
+    print(s.popSmallest()) -> 1
+    print(s.popSmallest()) -> 2
+    print(s.popSmallest()) -> 3
+    print(s.popSmallest()) -> 2
+    print(s.popSmallest()) -> 4
+    ...
+
+    
+'''
+
 
 class SmallestInfiniteSet:
     def __init__(self):
-        self._heapq = [i for i in range(1, 1001)]
-        heapify(self._heapq)
-        self._set = set([i for i in range(1, 1001)])
+        self._min_heap = [ i for i in range(1, 1001)]
+        heapq.heapify(self._min_heap)
+        self._set = set([ i for i in range(1, 1001)])
         
     def popSmallest(self) -> int:
-        tmp = heappop(self._heapq)
-        self._set.remove(tmp)
-        return tmp
+        ret = heapq.heappop(self._min_heap)
+        self._set.remove(ret)
+        return ret
         
+
     def addBack(self, num: int) -> None:
         if num in self._set:
             return
-
-        heappush(self._heapq, num)
         self._set.add(num)
-
-        
+        heapq.heappush(self._min_heap, num)
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
