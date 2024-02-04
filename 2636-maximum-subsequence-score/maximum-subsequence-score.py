81465d92b7573f4bcf05f0a1d91de8234f69c132
Wonhee Jang <Qtsky89@gmail.com>
2024-02-04 17:41:40 +0900

    Time: 792 ms (55.65%) | Memory: 36.4 MB (97.51%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.py
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.py
@@ -1,39 +1,56 @@
-# https://www.youtube.com/watch?v=ax1DKi5lJwk
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         '''
-        2 1 14 12
-        11 7 13 6
+        example1.
 
-        (2, 11) (1, 7) (14, 13) (12, 6)
-        after sort
-        (14, 13) (2, 11) (1, 7) (12, 6)
+        nums1 = [1, 3, 3, 2]
+        nums2 = [2, 1, 3, 4]
+        k = 3
+
+        get k element from nums1 * get min value from nums2 (k element)
+
+        want to get maximum value and return that value
+
+        nums1 => I want to get maximum value
+        nums2 => I want to avoid min value
+
+        [1, 2] [3, 1] [3, 3] [2, 4]
+
+        after sort by nums2
+      
+        [2, 4] [3, 3] [1, 2] [3, 1] 
+
+
+        case 2 is minimum
+        2, 3 => maximum 2 of the these value
+
+        case 1 is minimum
+        2, 3, 1  => maximum 2 of the these value
+
+        ..
         '''
 
-        r: tuple[int, int] = []
-
+        num_pack = []
         for i in range(len(nums1)):
-            r.append([nums1[i], nums2[i]])
-
-        r.sort(key=lambda x: x[1], reverse=True)
+            num_pack.append([nums1[i], nums2[i]])
         
-        max_ret = 0
-        tmp_sum = 0
+        num_pack.sort(key=lambda p:p[1], reverse=True)
+        # nums_pack =[ [2, 4] [3, 3] [1, 2] [3, 1] ]
+
         min_heap = []
-        for x, y in r:
+        tmp_sum = 0
+        max_ret = 0
+
+        for x, y in num_pack:
             heapq.heappush(min_heap, x)
             tmp_sum += x
 
             if len(min_heap) > k:
-                poped = heapq.heappop(min_heap)
-                tmp_sum -= poped
+                tmp_sum -= heapq.heappop(min_heap)
             
             if len(min_heap) == k:
                 max_ret = max(max_ret, tmp_sum * y)
-        
         return max_ret
-
-
-
-
-        return output
+                
+            
+        
