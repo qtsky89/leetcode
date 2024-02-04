9a96e485c88a25e8d887c7510ce7649d62d71447
Wonhee Jang <Qtsky89@gmail.com>
2024-02-04 21:38:34 +0900

    Time: 54 ms (26.37%) | Memory: 16.6 MB (76.93%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,29 +1,10 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         '''
-        https://www.youtube.com/watch?v=kMzJy9es7Hc
+        peak => if   nums[i-1]  < nums[i] > nums[i+1] 
+                if   i==0   and nums[i] > nums[i+1]
+                if   nums[i-1] <  nums[i] and i== len(nums)-1
 
-        nums = [1, 2, 3, 1]
-
-                      -V 
-                   -
-                -        -
-
-
-        nums = [1, 2, 1, 3, 5, 6, 4]
-                               -V
-                            -
-                                  -
-                         -
-                   -V    
-                -     -
-
-        
-        binary search
-        1. start_index, middle_index, end_index
-               1           3             4
-        2. check middle_index is peak
-        3. else search right!
         '''
 
         if len(nums) == 1:
@@ -32,20 +13,16 @@ class Solution:
         l, r = 0, len(nums)-1
 
         while l <= r:
-            m = (l + r) // 2
+            i = (l + r) // 2
 
-            # check middle is peak element
-            if (m == 0 or nums[m-1] < nums[m]) and (m == len(nums)-1 or nums[m] > nums[m+1]):
-                return m
-            
-            # it's not then, go right
-            if nums[m] < nums[m+1]:
-                l = m + 1
+            left = nums[i-1] if i-1 >= 0 else -float('inf')
+            right = nums[i+1] if i+1 <= len(nums)-1 else -float('inf')
+
+            # let check is's peak
+            if (left < nums[i] > right):
+                return i
+            # increasing
+            elif nums[i] < right:
+                l = i + 1
             else:
-                r = m - 1
-
-            
-            
-            
-
-        
+                r = i - 1
