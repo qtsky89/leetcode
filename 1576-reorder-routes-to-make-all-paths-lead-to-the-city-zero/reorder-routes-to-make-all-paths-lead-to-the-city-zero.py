aa040f0603405b0fd0758ef822bd5fcf383ed5f8
Wonhee Jang <Qtsky89@gmail.com>
2024-02-04 15:21:06 +0900

    Time: 905 ms (87.24%) | Memory: 42.7 MB (92.49%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,33 +1,98 @@
-from collections import deque
-
-# https://www.youtube.com/watch?v=m17yOR5_PpI
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        edges = {x: [] for x in range(n)}
-        for x, y in connections:
-            edges[x].append(y)
+        '''
+        connections[i] = [ai, bi]
 
-        neighbors = {city: [] for city in range(n)}
-        for x, y in connections:
-            neighbors[x].append(y)
-            neighbors[y].append(x)
+        a - > b
 
-        visited = set()
-        count = 0
+        city 0
 
+        example1.
+
+        0 <- 1 <- 3  <- 2
+          <- 4 -> 5
+
+
+        1. 1
+        2. 1
+        3. 0
+        4. 0
+        5. 1
+
+        result = 3
+
+        # step1 make hash map of connections
+        connections_map = {
+            0 : [1],
+            1 : [3],
+            2 : [3],
+            4 : [0, 5],
+        }
+
+        neighbor_map = {
+            0: [1, 4],
+            1 : [0],
+            1 : [3],
+            3 : [1, 2],
+            2 : [3]
+            4 : [0, 5]
+            5 : [4]
+        }
+
+        # step2 use queue and bfs, see we can go to the city zero,
+        if we hit the dead-end modify the route and increase the modfy_count
+
+        # step3 return modify_count
+        '''
+
+         # step1 make hash map of connections
+        connection_map = {}
+        neighbor_map = {}
+
+        for start, end in connections:
+            if start not in connection_map:
+                connection_map[start] = [end]
+            else:
+                connection_map[start].append(end)
+        
+            if start not in neighbor_map:
+                neighbor_map[start] = [end]
+            else:
+                neighbor_map[start].append(end)
+            
+            if end not in neighbor_map:
+                neighbor_map[end] = [start]
+            else:
+                neighbor_map[end].append(start)
+        
+        # step2 use queue and bfs, see we can go to the city zero,
+        # if we hit the dead-end modify the route and increase the modfy_count
+
+        # q = [0, 1, 2, 3, 4, 5]
         q = deque([0])
+        count = 0
+        visited = set()
+
+
         while q:
             # 0
             item = q.popleft()
+
             visited.add(item)
 
-            # neighbor = 1, 4 ...
-            for neighbor in neighbors[item]:
-                if neighbor not in visited:
-                    if not (neighbor in edges and item in edges[neighbor]):
+            for next_item in neighbor_map[item]:
+                # next_item = 1, 4
+                if next_item not in visited:
+                    if next_item not in connection_map or item not in connection_map[next_item]:
                         count += 1
-                    q.append(neighbor)
+                    q.append(next_item)
+
         return count
 
-        
-        
+            
+
+            
+
+            
+            
+
