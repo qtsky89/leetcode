87a2a91ad01cd53779f1a0149c8e1c518a4bd42f
Wonhee Jang <Qtsky89@gmail.com>
2024-02-06 00:40:33 +0900

    Time: 40 ms (32.20%) | Memory: 16.4 MB (75.14%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,9 +1,18 @@
 class Solution:
     def __init__(self):
-        self._hash_map = {0:0, 1:1, 2:1}
+        self._cache = {0: 0, 1: 1, 2: 1}
 
     def tribonacci(self, n: int) -> int:
-        if n not in self._hash_map:
-            self._hash_map[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
-        return self._hash_map[n]
+        '''
+        t(0) = 0
+        t(1) = 1
+        t(2) = 1
+
+        t(n+3) = t(n) + t(n+1) + t(n+2)
+        '''
+
+        if n not in self._cache:
+            self._cache[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        return self._cache[n]
+
         
