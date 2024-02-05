e5e4254f36f6d2667615e922a0a0d1af79e9f8ff
Wonhee Jang <Qtsky89@gmail.com>
2024-02-05 22:36:21 +0900

    Time: 39 ms (50.48%) | Memory: 16.6 MB (57.89%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,40 +1,29 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         '''
-        k=3, n=7
-
-        1 + 2 + 4 = 7
-
-        k=3, n=9
-
-        1, 2, 6
-        1, 3, 5
-        2, 3, 4
-    
+        q = [{4} {2} {3} {1} ...]
         '''
 
+
         q = deque()
-        
-        for i in range(1, n):
-            q.append([[i], i])
-              
+        for i in range(1, 10):
+            q.append([[i],i])
+
         ret = []
         while q:
-            item, total = q.popleft()
+            item, current_sum = q.popleft()
 
-            if len(item) == k and total == n:
+            if len(item) == k and current_sum == n:
                 ret.append(item)
                 continue
-            
-            if len(item) == n:
+            elif len(item) > k or current_sum > n:
                 continue
-
-            for i in range(item[-1]+1, 10):
-                if total + i <= n:
-                    q.append([[*item, i], total + i])
-        return ret
             
+            for i in range(item[-1]+1, 10):
+                q.append([[*item, i], current_sum + i])
 
+            
+        
+        return ret
 
-
-
+        
