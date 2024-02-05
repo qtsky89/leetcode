29ea1b113e4ca31115b8d1ebf8996881dbc7ad85
Wonhee Jang <Qtsky89@gmail.com>
2024-02-05 21:09:44 +0900

    Time: 297 ms (41.90%) | Memory: 21.3 MB (84.10%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,28 +1,27 @@
 '''
+example1.        
+    100 80 60 70 60 75 85 ...
+     1   1  1  2  1  4 6
 
-100 80 60 70 60 75  85 ...
-
-1   1   1  2  1  4  6
-
-
-stack
-100 85  
-1    6  
+stack = [100, 1] [85, 6] 
 '''
 
-# brute force
+
 class StockSpanner:
+
     def __init__(self):
-        self._stack = []
+        self.stack = []
 
     def next(self, price: int) -> int:
         total = 1
-        while self._stack and self._stack[-1][0] <= price:
-            stacked_price, count =  self._stack.pop()
-            total += count
+        while self.stack and self.stack[-1][0]  <= price:
+            _, span = self.stack.pop()
+            total += span
         
-        self._stack.append([price, total])
+        self.stack.append([price, total])
+
         return total
+        
         
         
 
