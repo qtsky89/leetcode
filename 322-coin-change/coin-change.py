5c5a4e6f7ccae7c88c89a663cf8c282adb2c9693
Wonhee Jang <Qtsky89@gmail.com>
2024-02-10 16:32:44 +0900

    Time: 735 ms (84.69%) | Memory: 16.9 MB (78.72%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,6 +1,9 @@
 class Solution:
+
+    # this is BFS solution
+    '''
     def coinChange(self, coins: List[int], amount: int) -> int:
-        '''
+        
         coins = [1, 2, 5] amount = 11
 
         => [1, 1, 1, 1, ....] => 11
@@ -15,7 +18,7 @@ class Solution:
         step2. in while loop, don't do when sum of coins is in visited
 
         step3. return length of current_coin when it reach the amount
-        '''
+        
 
         if amount == 0:
             return 0
@@ -43,4 +46,37 @@ class Solution:
                     visited.add(total + coins[i])
                     q.append([length+1, total + coins[i]])
         
+        return -1
+        '''
+
+    # https://youtu.be/H9bfqozjoqs?si=LVodgTqFrE5xl4fg dp solution
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        '''
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        dp[3] = 2
+        dp[4] = 2
+        dp[5] = 1
+        dp[6] = dp[5] + dp[1] ...
+
+        dp[n] = dp[n - coin] + 1
+        '''
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0 
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i-coin] +1)
+        
+        return dp[-1] if dp[-1] != float('inf') else -1
+
+
+
+
+
+
+
+
         return -1
