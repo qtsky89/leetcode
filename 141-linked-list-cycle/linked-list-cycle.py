6ce11b30fca7c4fb8136b8e3e0b4810e564229a6
Wonhee Jang <Qtsky89@gmail.com>
2024-02-18 01:01:00 +0900

    Time: 39 ms (91.74%) | Memory: 19.1 MB (68.22%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,25 +6,15 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        '''
-        3 -> 2 -> 0 -> -4
-        sf
-
-        1. if no cycle, s, f are not going to meet
-
-        2. if cycle, s, f will be meet
-        '''
-
         if not head:
             return False
-        
+
         s, f = head, head.next
 
         while f and f.next:
             if s == f:
                 return True
-            s, f = s.next, f.next.next
-        return False
-        
 
-        
+            s, f = s.next, f.next.next
+
+        return False
