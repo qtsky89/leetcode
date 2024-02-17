7d360a546318515795e17cd6792ca491edb8841b
Wonhee Jang <Qtsky89@gmail.com>
2024-02-17 20:26:16 +0900

    Time: 855 ms (15.65%) | Memory: 27.2 MB (97.61%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         '''
-        example1.
+        prices =     [ 7, 1, 5, 3, 6, 4]
 
-        prices =     [ 7 1 5 3 6 4 ]
-        max_values = [ 7 6 6 6 6 4 ]
-
-        step1. let's make max_values in reverse order
-        step2. let's find start increasing point
-        step3. let's find the max differ
+        max_prices = [ 7  6  6  6  6  4]
         '''
 
-        # step1. let's make max_values in reverse order
-        max_values = [0] * len(prices)
-        max_values[-1] = prices[-1]
-        for i in range(len(max_values)-2, -1, -1):
-            max_values[i] = max(prices[i], max_values[i+1])
+        max_prices = [0] * len(prices)
+        max_prices[-1] = prices[-1]
+
+        for i in range(len(max_prices)-2, -1, -1):
+            max_prices[i] = max(max_prices[i+1], prices[i])
         
-        # step2. let's find start increasing point
-        max_diff = 0
-        for i in range(len(prices)-1):
-            max_diff = max(max_diff, max_values[i] - prices[i])
+        max_profit = 0
+        for i in range(len(prices)):
+            max_profit = max(max_profit, max_prices[i] - prices[i])
         
-        return max_diff
+        print(max_prices)
+        
+        return max_profit
             
-        
