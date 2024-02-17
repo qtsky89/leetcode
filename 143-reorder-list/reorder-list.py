8850e8ff24d9e127f59096a520f9862de4ba0f37
Wonhee Jang <Qtsky89@gmail.com>
2024-02-18 00:42:49 +0900

    Time: 45 ms (96.61%) | Memory: 24.7 MB (75.87%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,50 +8,58 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        
         '''
-        1 -> 2 -> 3 <- 4 <- 5
-        ^         ^         ^
-    
-        1. find the middle man
-        2. after middle man change direction
-        3. merge!
+        1 -> 2 <- 3 <-  4
+        s    f
+             s           f
+            
+        1 -> 4 -> 2 -> 3
 
 
-        1 -> 2 -> 3 <- 4
-                  ^
-
-        1 -> 4 -> 2 -> 3 -> None
+        1. find the middle node
+        2. reverse the right nodes
+        3. merge
+        4. return head
         '''
 
-        # 1. find the middle man
+        ret = ListNode(None, None)
+        ret_p = ret # for adding the new node
 
+        # 1. find the middle node
         s, f = head, head.next
         while f and f.next:
-            s = s.next
-            f = f.next.next
+            s, f = s.next, f.next.next
         
-        # 2. after middle man change direction
-        prev, p = None, s
+        # 2. reverse the right nodes
+        # 1->2->3->4->5
 
+        prev, p = None, s
         while p:
             tmp = p.next
             p.next = prev
             prev, p = p, tmp
-
         
-        # 3. merge!
+        # 3. merge
         p1, p2 = head, prev
-        
+
         while p1 and p2:
             tmp1, tmp2 = p1.next, p2.next
-            
+
             p1.next, p2.next = p2, tmp1
 
             p1, p2 = tmp1, tmp2
-        
+
         return head
-        
-        
+
+
+            
+
         
 
         
+
+
+
+
+        return ret.next
