2e0aa4430dc04a2b9d74021245b3a8ede849d331
Wonhee Jang <Qtsky89@gmail.com>
2024-02-18 18:46:18 +0900

    Time: 67 ms (91.45%) | Memory: 19.8 MB (98.85%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,25 +1,35 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         '''
-        example1.
-            intervals = [[0,30],[5,10],[15,20]]
-        '''
+        min_end_heap = []
 
-        # intervals = [[0,30],[5,10],[15,20]]
-        intervals.sort()
 
-        # heap can have end values only
-        heap = []
-        max_heap_count = 0
-        for interval in intervals:
-            # there is ended meeting room
-            if heap and heap[0] <= interval[0]:
+        1. sort by start time
+        2. keep min_end_heap
+        3. whenever new interval comes in, check with min end value
+            if min_end_value <= new_interval_start:
+               i don't need meeting room
+            else
+               i need new meeting room.
+            update min_end_value
+        '''    
+
+        # 1. sort by start time
+        intervals.sort(key=lambda x:x[0])
+
+        # 2. keep min_end_heap
+        heap = [intervals[0][1]]
+
+        # 3. whenever new interval comes in, check with min end value
+        count = 1
+        for i in range(1, len(intervals)):
+            min_end_time = heap[0]
+            # i don't need meeting room
+            if min_end_time <= intervals[i][0]:
                 heapq.heappop(heap)
-            heapq.heappush(heap, interval[1])
-            
-            max_heap_count = max(max_heap_count, len(heap))
-        
-        return max_heap_count
-            
+            else: # i need new meeting room
+                count += 1
+            heapq.heappush(heap, intervals[i][1])
+        return count
 
-        
+                
