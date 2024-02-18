cb4e8144054c0dd107772cb1ed5245d39b937cf1
Wonhee Jang <Qtsky89@gmail.com>
2024-02-18 17:58:31 +0900

    Time: 75 ms (65.38%) | Memory: 19.8 MB (88.44%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,55 +1,21 @@
 class Solution:
-    # nlogn solution
-    '''
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
-        
-
-        ret: list[list] = []
-
-        for, while loop intervals
-
-            if interval[0] < newInterval:
-                push to ret
-            else:
-                merge or just push
-        
-        return ret
-
-        n log n solution
-        
-
+        # just add
         intervals.append(newInterval)
-        intervals.sort()
-
-        ret: list[list[int]] = []
-
-        for interval in intervals:
-            # no overlap
-            if not ret or ret[-1][1] < interval[0]:
-                ret.append(interval)
-            else: #overlap
-                ret[-1][1] = max(ret[-1][1], interval[1])
         
-        return ret'''
+        # sort by start
+        intervals.sort(key= lambda x:x[0])
 
-    # O(n) solution https://youtu.be/A8NUOmlwOlM
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
-
-        ret = []
-
-        for i in range(len(intervals)):
-            # there is no overlap
-            if newInterval[1] < intervals[i][0]: 
-                ret.append(newInterval)
-                return ret + intervals[i:]
-            
-            if newInterval[0] > intervals[i][1]:
-                ret.append(intervals[i])
+        ret = [intervals[0]]
+        # merge
+        for i in range(1, len(intervals)):
+            # is overlaped? then merge
+            if intervals[i][0] <= ret[-1][1]:
+                ret[-1][1] = max(intervals[i][1], ret[-1][1])
             else:
-                newInterval = ([min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])])
-        
-        ret.append(newInterval)
-
+                ret.append(intervals[i])
         return ret
+        
+
+        
+        
