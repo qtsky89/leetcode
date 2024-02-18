c3f552b1d33fb37c29a5c09fcbe0b389316ff444
Wonhee Jang <Qtsky89@gmail.com>
2024-02-18 15:59:17 +0900

    Time: 344 ms (98.61%) | Memory: 17.8 MB (35.90%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,73 +1,28 @@
 class Solution:
-
-    # this is BFS solution
-    '''
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        coins = [1, 2, 5] amount = 11
-
-        => [1, 1, 1, 1, ....] => 11
-           [1, 2, ....] =>
-           [5, 5, 1] => 
-
-        step1. using queue, check every possibility
-        [5, 5, 1] [5, 2] [5, 1]
-        [2] 
-        [1]
-
-        step2. in while loop, don't do when sum of coins is in visited
-
-        step3. return length of current_coin when it reach the amount
-        
-
         if amount == 0:
             return 0
 
-        inputs = []
-        for i in range(len(coins)):
-            inputs.append([1, coins[i]])
-
-        q = deque(inputs)
-
-        visited = set()
-
-        while q:
-            length, total = q.popleft()
-
-            visited.add(total)
-
-            if total == amount:
-                return length
-            elif total > amount:
-                continue
-            
-            for i in range(len(coins)):
-                if total + coins[i] not in visited and total + coins[i] <= amount:
-                    visited.add(total + coins[i])
-                    q.append([length+1, total + coins[i]])
-        
-        return -1
-        '''
-
-    # https://youtu.be/H9bfqozjoqs?si=LVodgTqFrE5xl4fg dp solution
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        '''
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 1
-        dp[3] = 2
-        dp[4] = 2
-        dp[5] = 1
-        dp[6] = dp[5] + dp[1] ...
-
-        dp[n] = dp[n - coin] + 1
-        '''
-
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0 
+        q = deque()
 
         for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] = min(dp[i], dp[i-coin] +1)
+            q.append([1, coin])
         
-        return dp[-1] if dp[-1] != float('inf') else -1
+        visited = set()
+        while q:
+            current_count, current_amount = q.popleft()
+            visited.add(current_amount)
+            if current_amount == amount:
+                return current_count
+            elif current_amount > amount:
+                continue
+            
+            for coin in coins:
+                if current_amount + coin not in visited:
+                    visited.add(current_amount + coin)
+                    q.append([current_count+1, current_amount + coin])
+        
+        return -1
+        
+
+        
