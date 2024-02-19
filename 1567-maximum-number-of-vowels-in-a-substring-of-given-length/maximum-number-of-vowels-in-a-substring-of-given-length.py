f64a30c5adcb26a58939a3758e39712ed215aac1
Wonhee Jang <Qtsky89@gmail.com>
2024-02-19 20:29:36 +0900

    Time: 145 ms (39.07%) | Memory: 17.3 MB (82.70%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,29 +1,29 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
+        '''
+        s = ' a b c i i i d e f ' k = 3
+              ^   ^
+                ^   ^                  ret=1
+                  ^   ^                ret=2
+                    ^   ^              ret=3
+        '''
+
         p1, p2 = 0, k-1
 
-        '''
-        a b c i i i d e f
-        ^   ^
-        '''
-
-        max_count = 0
-
-        tmp_count = 0
+        current_vowel_count = 0
         for i in range(0, k):
-            if s[i] in {'a', 'e', 'i', 'o', 'u'}:
-                tmp_count += 1
-        
-        while p2 <= len(s)-1:
-            max_count = max(max_count, tmp_count)
+            if s[i] in 'aeiou':
+                current_vowel_count += 1
 
-            if p2 + 1 <= len(s)-1 and s[p2+1] in {'a', 'e', 'i', 'o', 'u'}:
-                tmp_count += 1
-            if s[p1] in {'a', 'e', 'i', 'o', 'u'}:
-                tmp_count -=1
+        max_vowel_count = 0
+        while p2 <= len(s)-1:
+            max_vowel_count = max(max_vowel_count, current_vowel_count)
+            
+            if p2+1 <= len(s)-1 and s[p2+1] in 'aeiou':
+                current_vowel_count += 1
+            
+            if s[p1] in 'aeiou':
+                current_vowel_count -= 1
 
             p1, p2 = p1+1, p2+1
-        return max(max_count, tmp_count)
-
-
-        
+        return max_vowel_count
