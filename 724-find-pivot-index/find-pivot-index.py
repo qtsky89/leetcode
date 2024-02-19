efc8c6486e675aeadbea5342058314752e02a8fe
Wonhee Jang <Qtsky89@gmail.com>
2024-02-19 23:46:29 +0900

    Time: 137 ms (29.97%) | Memory: 18 MB (56.32%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,41 +1,37 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         '''
-        nums = [1, 7, 3, 6, 5, 6]
-
-        l_sum = [1,  8,  11, 17, 22, 28]
-        r_sum = [28, 27, 20, 17, 11, 6]
-
-        pivot[0] => left: 0,       right: r_sum[1]
-        pivot[1] => left: l_sum[0], right: r_sum[2]
-        pivot[2] => left: l_sum[1], right: r_sum[3]
-        pivot[3] => left: l_sum[2], right: r_sum[4]
-        pivot[4] => left: l_sum[3], right: r_sum[5]
-        pivot[5] => left: l_sum[4], right: 0
+        pivot index
+        nums =      [ 1, 7, 3, 6, 5, 6  ]
+        left_sum =  [ 0  1  8  11 17 22 ]                  
+     right_sum =    [ 27 20 17 11 6  0 ]
+                 ^                 0, 27
+                    ^              1, 20
+                       ^           8, 17
+                          ^        11  11 => return 3
+                                   17  6
+                                   22  0
         '''
 
-        l_sum = [0] * len(nums)
-        l_sum[0] = nums[0]
+        left_sum = [0] * len(nums)
+        right_sum = [0] * len(nums)
 
-        for i in range(1, len(l_sum)):
-            l_sum[i] = l_sum[i-1] + nums[i]
-
-        r_sum = [0] * len(nums)
-        r_sum[-1] = nums[-1]
-        for i in range(len(r_sum)-2, -1, -1):
-            r_sum[i] = r_sum[i+1] + nums[i]
+        left_sum[0] = nums[0]
+        for i in range(1, len(nums)):
+            left_sum[i] = left_sum[i-1] + nums[i]
         
-        for i in range(len(nums)):
-            left_sum = l_sum[i-1] if i-1 >= 0 else 0
-            right_sum = r_sum[i+1] if i+1 <= len(nums)-1 else 0
+        left_sum.insert(0, 0)
+        left_sum.pop()
+        
+        right_sum[-1] = nums[-1]
+        for i in range(len(nums)-2, -1, -1):
+            right_sum[i] = right_sum[i+1] + nums[i]
+        
+        right_sum.pop(0)
+        right_sum.append(0)
 
-            if left_sum == right_sum:
+        for i in range(len(nums)):
+            if left_sum[i] == right_sum[i]:
                 return i
         return -1
-            
-        
-
-
-        
-
         
