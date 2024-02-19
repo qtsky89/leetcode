1cbe67029d214b029e088f5b3ea5342d228e33dd
Wonhee Jang <Qtsky89@gmail.com>
2024-02-19 20:19:50 +0900

    Time: 939 ms (23.11%) | Memory: 28.3 MB (80.65%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,30 +1,46 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        p = 0
         '''
-        k = 4
+        [1, 12, -5, -6, 50, 3] k = 4
+         p1          p2
+            p1          p2
+                 p1         p2
 
-        sum[i:i+k] = accumuate_sum[0+k-1] - accumulate_sum[i-1]
-
-        [1, 12, -5, -6, 50, 3]
-         1  13  8   2   52  53
-
-         0  1   2   3   4   5
+        [1, 13, 8, 2, 52, 55]
+         ^             ^ => 51 /4
 
 
-        [            ]
-            [            ]
-               [            ]   2+4 
+        12, -5, -6, 50
+
+        1. make the accumulate sum array
+        2. find the sum in that window
+        3. divide by k
+        4. return maximum result
         '''
-
+        # 1. make the accumulate sum array
+        # [1, 13, 8, 2, 52, 55]
         accumulate_sum = [0] * len(nums)
-        accumulate_sum[0] = nums[0]
-        for i in range(1, len(nums)):
-            accumulate_sum[i] = accumulate_sum[i-1] + nums[i]
 
-        max_val = -float('inf')
-        while p + k <= len(nums):
-            sum_data = accumulate_sum[p+k-1] - (accumulate_sum[p-1] if p-1 >= 0 else 0)
-            max_val = max(max_val, sum_data / k)
-            p += 1
-        return max_val
+        accumulate_sum[0] = nums[0]
+        for i in range(1, len(accumulate_sum)):
+            accumulate_sum[i] = accumulate_sum[i-1] + nums[i]
+        
+        # 2. find the sum in that window
+        p1, p2 = -1, k-1
+
+        ret = -float('inf')
+        while p2 <= len(nums)-1:
+            if p1 == -1:
+                ret = max(ret, accumulate_sum[p2] /k)
+            else:
+                ret = max(ret, (accumulate_sum[p2] - accumulate_sum[p1]) / k)
+
+            p1, p2 = p1+1, p2+1
+
+        return ret
+
+        
+
+        
+
+
