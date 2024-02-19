d2b9752c72e88c01e59d3895e9a10d0344e90f47
Wonhee Jang <Qtsky89@gmail.com>
2024-02-19 19:59:32 +0900

    Time: 534 ms (50.46%) | Memory: 29.4 MB (88.99%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,26 +1,24 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
-        11. Container with most water
+        (p2 - p1) * min(height[p1], height[p2])
 
-        1. formuala to water amount = > min(height[i], height[j]) * (j-i)
-        
-        2. if height[i] < height[j]: i+=1
-            else:
-                j-=1 until i < j
+        height = [ 1, 8, 6, 2, 5, 4, 8, 3, 7 ]
+                   p1                      p2
+                      p1                   p2
+
+                      if height[p1] < height[p2] p1 += 1
+                      else p2 -=1
         '''
 
-        i, j = 0, len(height)-1
+        p1, p2 = 0, len(height)-1
 
-        max_value = 0
-        while i < j:
-            max_value = max(max_value, min(height[i], height[j]) * (j-i))
+        ret = 0
+        while p1 < p2:
+            ret = max(ret, (p2 - p1) * min(height[p1], height[p2]))
 
-            if height[i] < height[j]:
-                i += 1
+            if height[p1] < height[p2]:
+                p1 += 1
             else:
-                j -= 1
-        return max_value
-
-
-        
+                p2 -= 1
+        return ret
