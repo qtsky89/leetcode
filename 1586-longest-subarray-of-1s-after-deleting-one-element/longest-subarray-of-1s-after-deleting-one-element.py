a60e4cdeeb0d7a5c1bdd2018d108bfba0a9313fe
Wonhee Jang <Qtsky89@gmail.com>
2024-02-19 23:01:08 +0900

    Time: 517 ms (16.34%) | Memory: 20.2 MB (65.30%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,27 +1,36 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         '''
-
-        nums = [0,1,1,1,0,1,1,0,1]
-               ^^
+        example1. nums = [1, 1, 0, 1] => [1, 1, 1]
+                                ^     => 3
+        
+        exmaple2. nums = [1, 1, 1, 1, 0, 1, 1, 0, 1 ] => 5
+                          ^           ^
+                             ^                 ^
+                                         ^        ^
+        example3. nums = [ 1, 1, 1 ] => 2  -> TODO. edgecase 
+                                 ^  
+        1. remove 0
+        2. if that 0 can make longest 1's sub array prefer that 1
+        3. remove 1
         '''
 
         p1, p2 = 0, 0
-
-        k = 1
-        count = 0
-        while p2 <= len(nums)-1:
-            if nums[p2] == 0:
-                if k > 0:
-                    k -= 1
-                    p2 += 1
-                else:
-                    if nums[p1] == 0:
-                        k += 1
-                    p1 += 1    
-            else:
-                p2 += 1
         
-            count = max(count, p2-p1-1)
+        coin = 1 # this coin can change 0 -> 1
 
-        return count
+        if nums[0] == 0:
+            coin -=1
+        
+        ret = 0
+        while p2 <= len(nums)-1:
+            ret = max(ret, p2-p1+1)
+            if p2 + 1 <= len(nums)-1 and nums[p2+1] == 0:
+                coin -= 1
+            while coin < 0: # I need my coin back
+                if nums[p1] == 0:
+                    coin += 1 
+                p1 +=1
+            p2 += 1
+        return ret-1
+        
