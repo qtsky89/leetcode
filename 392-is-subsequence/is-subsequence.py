69f9434aa9aa317dae68474c89e9fd2ac9ba08ee
Wonhee Jang <Qtsky89@gmail.com>
2024-02-19 19:45:59 +0900

    Time: 42 ms (25.94%) | Memory: 16.6 MB (70.31%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,18 +1,24 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         '''
-        s = 'abc', t = 'ahbgdc'
-
-        len(s) <= len(t)
+        s = 'a b c'  t = 'a h b g d c'
+             ^            ^
+               ^            ^
+               ^              ^
+                   ^                  ^
+        
+        s = 'a x c'  t = 'a h b g d c'
+                          ^         ^ False
         '''
+
+        if not s:
+            return True
 
         p1, p2 = 0, 0
 
-        while p1 <= len(s)-1 and p2 <= len(t) - 1:
+        while p1 <= len(s)-1 and p2 <= len(t) -1:
             if s[p1] == t[p2]:
                 p1 += 1
-                p2 += 1
-            else:
-                p2 += 1
-
+            p2 += 1
+        
         return p1 == len(s)
