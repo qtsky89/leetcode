859cfb3d55648bc8ce10093ad974a796aad27794
Wonhee Jang <Qtsky89@gmail.com>
2024-02-20 17:35:43 +0900

    Time: 37 ms (77.93%) | Memory: 18.6 MB (87.15%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -7,38 +7,49 @@ class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         '''
         example1.
-            1 - > 2 -> 3 -> 4 -> 5
-         head1   head2
-            ^1    ^2
-                       ^1   ^2   
-                                 ^1  ^2
 
+        ret -> 0 ->
+        output: 0 -> 2 -> 4 -> 1 -> 3
 
-            ^1.next = head2
+        input:  0 -> 1 -> 2 -> 3 -> 4
+                ^1   ^2
+                          ^1   ^2
+        output: 0 -> 2 -> 4 -> 1 -> 3
 
-            1 -> 3 -> 5 -> 2 -> 4
-        
         example2.
-            1  -> 2
-          head1   head2
+        input:  0
+        output: 0
 
+        example3.
+        input:  0 -> 1
+        output: 0 -> 1
+
+        example4.
+        input:  0 -> 1 -> 2
+                ^    ^
+                          ^   ^
+         
+        output: 0 -> 2 -> 1
+
+        example5.
+        input:  0 -> 1 -> 2 -> 3
+                ^    ^
+                          ^    ^
+
+        output: 0 -> 2 -> 1 -> 3
         '''
 
-        if not head or not head.next:
-            return head
-        
-        head1, head2 = head, head.next
+        if not head:
+            return None
 
-        p1, p2 = head1, head2
+        tmp = head.next
+        p1, p2 = head, head.next
+
         while p2 and p2.next:
-            p1.next = p1.next.next
-            p2.next = p2.next.next
-
-            p1 = p1.next
-            p2 = p2.next
+            p1.next, p2.next = p1.next.next, p2.next.next
+            p1, p2 = p1.next, p2.next
         
-        p1.next = head2
+        p1.next = tmp
 
-        return head1
-        
-            
+        return head
+
