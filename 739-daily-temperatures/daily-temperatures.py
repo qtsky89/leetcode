a6785c9f3e8564bd7e8820770650819051e5d0b9
Wonhee Jang <Qtsky89@gmail.com>
2024-02-20 22:51:10 +0900

    Time: 904 ms (50.18%) | Memory: 31.2 MB (59.06%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -2,23 +2,34 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         '''
         temperatures = [ 73 74 75 71 69 72 76 73 ]
-                         1   1  4  2  1  1  0  0
+                          0  1  2  3  4  5  6  7
 
-        ret = [1, 1, 4  2 1  1      ]
-               0  1  2  3 4  5 6  7
+        stack        = [ 75 71 72
+
+
+        ret = [0 0 0 0 0 0 0 0]
+
         '''
 
-        stack = []
         ret = [0] * len(temperatures)
 
-        for i in range(len(temperatures)):
-            while stack and temperatures[i] > temperatures[stack[-1]]:
+        stack = []
+        
+        i = 0 
+        while i <= len(temperatures)-1:
+            if not stack or temperatures[stack[-1]] > temperatures[i]:
+                stack.append(i)
+                i+=1
+                continue
+            
+            while stack and temperatures[stack[-1]] < temperatures[i]:
                 index = stack.pop()
-
                 ret[index] = i - index
             
             stack.append(i)
-
+            
+            i+=1
+        
         return ret
             
             
