043d7ace4515b975f9f41809c633a927071a13f8
Wonhee Jang <Qtsky89@gmail.com>
2024-02-20 18:54:27 +0900

    Time: 34 ms (76.67%) | Memory: 16.5 MB (93.18%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -7,36 +7,28 @@
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         '''
-            3
-        5       1
-      6  2     9  8
-      
-        7  4
-
-        def leaf node:  there is no child node
-
-
-        step 1. preorder travel each tree
-        step 2. if leaf add to list
-        step 3. compare two list
+        1. I have to implement dfs function
+        2. using dfs both of trees, and make leaf array
+        3. compare those leaf array
+        4. it's same return True, else False
         '''
 
-        def dfs(node: TreeNode, leafs: list[int]):
+        # 1. I have to implement dfs function
+        def dfs(node: TreeNode, leaf_arr: list[int]):
             if not node:
                 return
 
-            # if it's leaf
+            # leaf
             if not node.left and not node.right:
-                leafs.append(node.val)
+                leaf_arr.append(node.val)
             
-            dfs(node.left, leafs)
-            dfs(node.right, leafs)
-
-        leafs1: list[int] = []
-        leafs2: list[int] = []
-
-        dfs(root1, leafs1)
-        dfs(root2, leafs2)
-
-        return leafs1 == leafs2
+            dfs(node.left, leaf_arr)
+            dfs(node.right, leaf_arr)
         
+        leaf_arr1, leaf_arr2 = [], []
+
+        dfs(root1, leaf_arr1)
+        dfs(root2, leaf_arr2)
+
+        return leaf_arr1 == leaf_arr2
+
