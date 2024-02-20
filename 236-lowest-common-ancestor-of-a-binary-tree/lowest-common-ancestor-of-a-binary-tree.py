d697ec661701286aa17c3d633337e52bd7cf5dae
Wonhee Jang <Qtsky89@gmail.com>
2024-02-20 21:15:35 +0900

    Time: 55 ms (39.27%) | Memory: 21.2 MB (53.97%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -8,60 +8,37 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         '''
-        example1.
-
-                 3
-                
-            5(p)     1(q)
-             |
-          6   2    0    8
-               |
-            7   4
-
-        step1. make these hash function
-        {  
-            4 : 2
-            2 : 5
+        parent_map = {
             5 : 3
+            6 : 5 
         }
-
-        step2. make these list
-        4 -> 2 -> 5V -> 3
-        5V -> 3
-
-        step3. find common anccestor
         '''
 
-        # step1
-        hash_map = {}
-
+        parent_map = {root: None}
         def dfs(node: TreeNode):
-            if not node:
+            if not root:
                 return
             
             if node.left:
-                hash_map[node.left] = node
+                parent_map[node.left] = node
                 dfs(node.left)
+            
             if node.right:
-                hash_map[node.right] = node
+                parent_map[node.right] = node
                 dfs(node.right)
-        
-        hash_map[root] = None
+
         dfs(root)
 
-        #step2. make these list
-
         visited = set()
+        i = p
+        while i:
+            visited.add(i)
+            i = parent_map[i]
 
-        while p:
-            if p in visited:
-                return p
-            visited.add(p)
-            p = hash_map[p]
-        
-        while q:
-            if q in visited:
-                return q
-            visited.add(q)
-            q = hash_map[q]
-        return None
+        i = q
+        while i:
+            if i in visited:
+                return i
+            i = parent_map[i]
+
+
