c013dc9e9ab288f3bb6d5c42748cff51857e755c
Wonhee Jang <Qtsky89@gmail.com>
2024-02-20 15:53:01 +0900

    Time: 96 ms (17.94%) | Memory: 18 MB (51.03%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,33 +1,36 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         '''
-        asteroids = [5, 10, 15, -5, -5, -25, 30]
-                                         ^       
-        
-        stack = [-25, 30]
+        asteroids = [5, 10, -5]
         '''
 
-        stack = []
-        
         i = 0
-        while i<= len(asteroids)-1:
-            if not stack or asteroids[i] > 0:
-                stack.append(asteroids[i])
-                i+=1
-                continue
 
-            if (asteroids[i] < 0 and stack[-1] > 0) and (stack[-1] + asteroids[i]) > 0:
-                i+=1
-                continue
-            elif (asteroids[i] < 0 and stack[-1] > 0) and (stack[-1] + asteroids[i]) == 0:
-                stack.pop()
-                i+=1
-                continue
-            elif (asteroids[i] < 0 and stack[-1] > 0) and (stack[-1] + asteroids[i]) < 0:
-                while stack and (asteroids[i] < 0 and stack[-1] > 0) and (stack[-1] + asteroids[i]) < 0:
-                    stack.pop()
-            else:
+        stack = []
+        while i <= len(asteroids)-1:
+            if not stack:
                 stack.append(asteroids[i])
                 i+=1
+                continue
+            
+            if ((stack[-1] > 0 and asteroids[i] > 0) or
+                (stack[-1] < 0 and asteroids[i] < 0) or
+                (stack[-1] < 0 and asteroids[i] > 0)
+            ):
+                stack.append(asteroids[i])
+                i += 1
+                continue
+            
+            # collition
+            if abs(stack[-1]) == abs(asteroids[i]):
+                stack.pop()
+                i += 1
+                continue
+            elif abs(stack[-1]) > abs(asteroids[i]):
+                i += 1
+                continue
+            
+            while stack and stack[-1] > 0 and asteroids[i] < 0 and abs(stack[-1]) < abs(asteroids[i]):
+                stack.pop()
+            
         return stack
-                   
