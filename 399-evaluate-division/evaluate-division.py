e83a00f8bd353e0cc85fb083fc5c2660dce21714
Wonhee Jang <Qtsky89@gmail.com>
2024-04-02 01:19:49 +0900

    Time: 36 ms (58.69%) | Memory: 16.9 MB (5.68%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,76 +1,55 @@
-from collections import deque
-
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         '''
-        example1.
-
-        a / b , b / c
-        2.0       3.0
-
-        a / c, b /a, a/ e, a / a, x / x
-
-        we couldn't get them answer return -1
-
-        
         a / b = 2
         b / c = 3
 
-        a / c = 6
+        queries 
+        a / c
+        b / a
+        a / e
+        a / a
+        x / x
 
-        # step1. make these hash_map
+
         {
-           a : [ [b, 2.0], [d, 5.0] ]
-           b : [ [c, 3.0] ]
+            a : [(b, 2)]
+            b : [(a, 1/2), (c, 3)]
+            c : [(a, 1/3)]
         }
-
-        # step2. process every query using bfs
-        a -> b -> c
-
-        # step3. if we found end value, add to the result, if we coudn't return -1
         '''
 
-        # step1. make these hash_map
-        hash_map = {}
-        # start = a, end = b
+        hash_map = defaultdict(list)
+
         for i in range(len(equations)):
             start, end = equations[i]
-            if start in hash_map:
-                hash_map[start].append([end, values[i]])
-            else:
-                hash_map[start] = [[end, values[i]]]
-            
-            if end in hash_map:
-                hash_map[end].append([start, 1/values[i]])
-            else:
-                hash_map[end] = [[start, 1/values[i]]]
+            value = values[i]
+
+            hash_map[start].append((end, value))
+            hash_map[end].append((start, 1/value))
+                
+        q = deque()
+
+        ret = [-1.0] * len(queries)
+
+        for index, (start, end) in enumerate(queries):
+            q.append([start, end, start, 1.0, set(), index])
         
-        # step2. process every query using bfs
-        for i in range(len(queries)):
-            queries[i].append(1)
-            queries[i].append(i)
-            queries[i].append(set())
-
-        ret = [-1] * len(queries)
-        q = deque(queries)
         while q:
-            # key = a, end_value = c, current = 1
-            key, end_value, current, i, visited =  q.popleft()
+            start, end, current_var, current_data, visited, index = q.popleft()
+            visited.add(current_var)
 
-            if key in hash_map and key == end_value:
-                ret[i] = current
+            # end!
+            if current_var == end and current_var in hash_map:
+                ret[index] = current_data
                 continue
-            visited.add(key)
-
-            if key in hash_map:
-                for next_key, next_value in hash_map[key]:
-                    if next_key not in visited:
-                        q.append([next_key, end_value, current * next_value, i, set(visited)])
-
+            
+            if current_var in hash_map:
+                for next_var, next_data in hash_map[current_var]:
+                    if next_var not in visited:
+                        q.append([start, end, next_var, current_data*next_data, set(visited), index])
+            
         return ret
 
-
-
-
-
+            
 
