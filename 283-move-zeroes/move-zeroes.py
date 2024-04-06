9f5d98b5d9f92de8ea6022f757d9f861abbdd020
Wonhee Jang <Qtsky89@gmail.com>
2024-04-06 15:39:19 +0900

    Time: 121 ms (83.27%) | Memory: 18.2 MB (31.31%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,16 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         '''
-        Input: nums = [ 1, 3, 12,  0, 0 ]
-                              l       r
-        Output:       [ 1, 3, 12, 0, 0 ]
+        nums = 1   3   0   0   12
+                       ^        ^
+
         '''
 
         l, r = 0, 0
-
         while r <= len(nums)-1:
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
+
             r += 1
-        
