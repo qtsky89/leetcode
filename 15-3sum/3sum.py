a74a5fdd8331be48766eba944d71b2256755b071
Wonhee Jang <Qtsky89@gmail.com>
2024-04-06 19:38:04 +0900

    Time: 2639 ms (5.01%) | Memory: 21.2 MB (12.87%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,30 +1,36 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        '''
-        nums = [-4  -1  -1  0  1  2]
-                i    j            k => 4 add result
-                     i   j        k
-                            i   j  k
+        nums.sort()
+
         '''
 
-        nums.sort()
+        -4 -1 -1 0 1 2
+         ^  
+        
+
+        '''
 
         ret = []
         visited = set()
         for i in range(len(nums)-2):
-            j = i+1
+            j = i + 1
             k = len(nums)-1
 
-            # nums[j] + nums[k] == -nums[i] (should be)
             while j < k:
-                if nums[j] + nums[k] == -nums[i]:
-                    if (nums[i], nums[j], nums[k]) not in visited:
+                tmp = nums[j] + nums[k]
+
+                # bingo
+                if tmp == -nums[i]:
+                    key = ','.join([str(nums[i]), str(nums[j]), str(nums[k])])
+                    if  key not in visited:
                         ret.append([nums[i], nums[j], nums[k]])
-                        visited.add((nums[i], nums[j], nums[k]))
-                    j, k = j + 1, k - 1
-                elif nums[j] + nums[k] > -nums[i]:
+                        visited.add(key)
+                    j += 1
+                    k -= 1
+                elif tmp > -nums[i]:
                     k -= 1
                 else:
                     j += 1
-     
         return ret
+
+                
