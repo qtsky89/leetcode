d104cbcaaab61f477d16b1aaee15baea722a9055
Wonhee Jang <Qtsky89@gmail.com>
2024-04-07 14:02:32 +0900

    Time: 871 ms (36.98%) | Memory: 42.1 MB (11.09%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,28 +1,50 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        '''
+        coins = 1, 2, 5 amount = 13
+                5 + 5 + 2 + 1
+    
+        dp[13] => dp[8] + 1
+               => dp[11] + 1
+               => dp[12] + 1
+        
+
+        1 2 3 4 5 6 7 8 9 10 11 12 13
+                                    ^
+                      ^
+            ^
+        ^
+        '''
+
+        '''
+        {
+            1 : 1
+            2 : 1
+            5 : 1
+        }
+        '''
+
         if amount == 0:
             return 0
 
-        q = deque()
+        dp = {coin: 1 for coin in coins}
 
-        for coin in coins:
-            q.append([1, coin])
-        
-        visited = set()
-        while q:
-            current_count, current_amount = q.popleft()
-            visited.add(current_amount)
-            if current_amount == amount:
-                return current_count
-            elif current_amount > amount:
-                continue
+        def dfs(n: int) -> int:
+            if n in dp:
+                return dp[n]
             
+            candidates = []
             for coin in coins:
-                if current_amount + coin not in visited:
-                    visited.add(current_amount + coin)
-                    q.append([current_count+1, current_amount + coin])
-        
-        return -1
-        
+                if n - coin >= 0:
+                    tmp = dfs(n - coin)
+                    if tmp >= 0:
+                        candidates.append(tmp + 1)
+            
+            if candidates:
+                dp[n] = min(candidates)
+            else:
+                dp[n] = -1
 
+            return dp[n]
         
+        return dfs(amount)
