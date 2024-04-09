7455cf1e14336692e6ce432f8359e00caf359e6d
Wonhee Jang <Qtsky89@gmail.com>
2024-04-09 21:48:44 +0900

    Time: 47 ms (75.03%) | Memory: 21.1 MB (21.06%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,38 +7,62 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        '''
-        parent_map = {
-            5 : 3
-            6 : 5 
-        }
-        '''
+        # key: child, value: parent
+        parent_map: dict[TreeNode, TreeNode] = {}
 
-        parent_map = {root: None}
-        def dfs(node: TreeNode):
-            if not root:
+        def dfs(node: TreeNode | None) -> None:
+            if not node:
                 return
             
             if node.left:
                 parent_map[node.left] = node
                 dfs(node.left)
-            
             if node.right:
                 parent_map[node.right] = node
                 dfs(node.right)
 
+        parent_map[root] = None
         dfs(root)
 
         visited = set()
-        i = p
-        while i:
-            visited.add(i)
-            i = parent_map[i]
+        while p and q:
+            if p == q:
+                return p
+            elif p in visited:
+                return p
+            elif q in visited:
+                return q
 
-        i = q
-        while i:
-            if i in visited:
-                return i
-            i = parent_map[i]
+            visited.add(p)
+            visited.add(q)
 
+            if p in parent_map:
+                p = parent_map[p]
+            else:
+                p = None
+            if q in parent_map:
+                q = parent_map[q]
+            else:
+                q = None
+        
+        while p:
+            if p in visited:
+                return p
+            else:
+                visited.add(p)
 
+            if p in parent_map:
+                p = parent_map[p]
+            else:
+                p = None
+
+        while q:
+            if q in visited:
+                return q
+            else:
+                visited.add(q)
+
+            if q in parent_map:
+                q = parent_map[q]
+            else:
+                q = None
