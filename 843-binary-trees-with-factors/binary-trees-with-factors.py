1607115f32c1bd332921a71e5bc6aa8c00a9f400
Wonhee Jang <Qtsky89@gmail.com>
2024-04-10 01:07:17 +0900

    Time: 443 ms (26.16%) | Memory: 16.7 MB (98.67%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.py
+++ 843-binary-trees-with-factors/binary-trees-with-factors.py
@@ -1,14 +1,54 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
-        MOD = 10**9 + 7
+        '''
+        2 3 6 18
+               ^
+
+        dp[2] = 1
+        dp[3] = 1
+        dp[6] = 1 + 2 = 3
+        dp[18] = 1 + 6 => 7
+
+        
+
+        '''
+
         arr.sort()
-        dp = {}
-        for num in arr:
-            dp[num] = 1
-            for i in range(2, int(num ** 0.5) + 1):
-                if num % i == 0:
-                    j = num // i
-                    if i in dp and j in dp:
-                        dp[num] += dp[i] * dp[j] * (1 if i == j else 2)
-            dp[num] %= MOD
-        return sum(dp.values()) % MOD
+
+        # key : arr[i], value: int
+        dp = {n: 1 for n in arr}
+
+        for i in range(1, len(arr)):
+            j = 0
+            k = i-1
+
+            while j <= k:
+                if arr[i] == arr[j] * arr[k]:
+                    tmp = dp[arr[j]] * dp[arr[k]]
+
+                    if arr[j] != arr[k]:
+                        tmp *= 2
+                    
+                    dp[arr[i]] += tmp
+
+                    j += 1
+                    k -= 1
+                elif arr[i] > arr[j] * arr[k]:
+                    j += 1
+                else:
+                    k -= 1
+                    
+        return sum(dp.values()) % (10 ** 9 + 7)
+        '''
+        dp[2] = 1 = 1
+        dp[4] = 1 + 1 = 2
+        dp[5] = 1 = 1
+        dp[10] = 1 + 2 = 3
+        '''
+
+            
+            
+
+        
+
+        
