d187ec2e41038cd365a416eb24c4c3357640655d
Wonhee Jang <Qtsky89@gmail.com>
2024-04-10 21:07:31 +0900

    Time: 42 ms (12.68%) | Memory: 16.5 MB (89.22%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,50 +1,32 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        '''
-        digits = "23"
-
-        step1. make hash map
-        {
-            2 : [a,b,c]
-            3
-            4
-            5
-            6
-            7
-            8
-            9
-        }
-        '''
-        if digits == "":
+        if not digits:
             return []
 
-        digit_letter_map = {
-            '2' : ['a', 'b', 'c'],
-            '3' : ['d', 'e', 'f'],
-            '4' : ['g', 'h', 'i'],
-            '5' : ['j', 'k', 'l'],
-            '6' : ['m', 'n', 'o'],
-            '7' : ['p', 'q', 'r', 's'],
-            '8' : ['t', 'u', 'v'],
-            '9' : ['w', 'x', 'y', 'z'],
+        dic = {
+            '2' : 'abc',
+            '3' : 'def',
+            '4' : 'ghi',
+            '5' : 'jkl',
+            '6' : 'mno',
+            '7' : 'pqrs',
+            '8' : 'tuv',
+            '9' : 'wxyz'
         }
 
-        q = deque([""])
-
         ret = []
-        while q:
-            item = q.popleft()
 
-            if len(item) == len(digits):
-                ret.append(item)
-                continue
-            
-            # len(item) => 0
+        def dfs(current: str) -> None:
+            if len(current) == len(digits):
+                ret.append(current)
+                return
 
-            # char => a, b, c...
-            i = len(item)
-            for char in digit_letter_map[digits[i]]:
-                q.append(item + char)
+            i = len(current)
+
+            key = digits[i]
+            for value in dic[key]:
+                dfs(current + value)
+
+        dfs('')
+
         return ret
-        
-            
