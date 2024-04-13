7d3a6a8e4ad67546b2ecf13d38e00b3817f3e697
Wonhee Jang <Qtsky89@gmail.com>
2024-04-13 21:29:02 +0900

    Time: 74 ms (68.53%) | Memory: 18.9 MB (20.54%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,24 +1,49 @@
 class Solution:
     def minRemoveToMakeValid(self, strings: str) -> str:
-        ret = []
-        open_count = 0
+        '''
+        l e e t () t ( c ) o ) d e (
+                                  
+        open_counter = 0
 
-        for s in strings:
-            if s == '(':
-                open_count += 1
-                ret.append(s)
-            elif s == ')' and open_count > 0:
-                open_count -= 1
-                ret.append(s)
-            elif s != ')':
-                ret.append(s)
+        if (
+            open_counter += 1
+            stack.append()
+        if char:
+            stack.append()
         
-        filtered = deque()
-        for i in range(len(ret)-1, -1, -1):
-            if ret[i] == '(' and open_count > 0:
-                open_count -= 1
-                continue
+        if )
+            if open_counter > 0
+                open_coutner -=1
             else:
-                filtered.appendleft(ret[i])
+                conintue
         
-        return ''.join(filtered)
+        if open_counter > 0:
+            reverse order I should remove open parathesis
+
+        '''
+
+        open_counter =  0
+
+        stack: list[str] = []
+        for s in strings:
+            if s.isalpha():
+                stack.append(s)
+            elif s == '(':
+                open_counter += 1
+                stack.append(s)
+            else: # s== ')'
+                if open_counter > 0:
+                    open_counter -= 1
+                    stack.append(s)
+                else:
+                    continue # skip
+        
+        ret = []
+        for i in range(len(stack)-1, -1, -1):
+            if open_counter > 0 and stack and stack[-1] == '(':
+                open_counter -= 1
+                stack.pop()
+            else:
+                ret.append(stack.pop())
+        return ''.join(ret[::-1])
+            
