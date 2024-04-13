899b4aa8a29af5b41bc0ff03a179b0147535db6f
Wonhee Jang <Qtsky89@gmail.com>
2024-04-13 21:55:29 +0900

    Time: 221 ms (11.87%) | Memory: 27.9 MB (21.74%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -1,29 +1,33 @@
 class UndergroundSystem:
+    '''
+    session = {
+        'id' : (stationName, startTime)
+    }
+
+    hash_table = {
+        (startStation, endStation) : [t1, t2, t3]
+    }
+
+    '''
 
     def __init__(self):
-        # key = id, value = (station, t)
-        # {45: ("Leyton", 3)}
-        self._session = {}
-
-        # key = (stationName, stationName), value = [diff1, diff2 ...]
-        # {("Paradice", "London"): [40, 35, 20...]}
-        self._times = {}
+        self.session = {}
+        self.times = {}
+        
 
     def checkIn(self, id: int, start_station: str, start_time: int) -> None:
-        self._session[id] = (start_station, start_time)
-
+        self.session[id] = (start_station, start_time)
+        
     def checkOut(self, id: int, end_station: str, end_time: int) -> None:
-        start_station, start_time = self._session[id]
-        del self._session[id]
+        start_station, start_time = self.session[id]
 
-        if (start_station, end_station) in self._times:
-            self._times[(start_station, end_station)].append(end_time - start_time)
+        if (start_station, end_station) in self.times:
+            self.times[(start_station, end_station)].append(end_time-start_time)
         else:
-            self._times[(start_station, end_station)] = [end_time - start_time]
+            self.times[(start_station, end_station)]  = [end_time-start_time]
 
     def getAverageTime(self, start_station: str, end_station: str) -> float:
-        return sum(self._times[(start_station, end_station)]) / len(self._times[(start_station, end_station)])
-        
+        return sum(self.times[(start_station, end_station)])/len(self.times[(start_station, end_station)])
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
