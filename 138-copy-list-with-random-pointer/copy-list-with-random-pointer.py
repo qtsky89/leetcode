8debd272229db36c069fb85ae0bbbec69ba3282b
Wonhee Jang <Qtsky89@gmail.com>
2024-04-14 17:49:27 +0900

    Time: 36 ms (72.99%) | Memory: 17.4 MB (32.10%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,36 +9,44 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        '''
-        7 -> 13 -> 11 -> 10 -> 1
-
-        7 -> 7' -> 13 -> 13' -> 11 -> 11' -> 10 -> 10' -> 1 -> 1'
-        '''
 
         if not head:
             return None
+        '''
+        step1 make this
+        7 -> 7` -> 13 -> 13` -> 11 -> 11` -> 10 -> 10` -> 1 -> 1`-> None
+
+        step2.copy random pointer
+
+        '''
+        # step1 make this
+        # 7 -> 7` -> 13 -> 13` -> 11 -> 11` -> 10 -> 10` -> 1 -> 1`-> None
 
         p = head
-
         while p:
-            tmp = p.next
-            node = Node(p.val)
-            p.next = node
-            node.next = tmp
-            p = tmp
-        '''
-        7 -> 7' -> 13 -> 13' -> 11 -> 11' -> 10 -> 10' -> 1 -> 1'
-        '''
+            next_node = p.next
+            tmp = Node(p.val)
+
+            p.next = tmp
+            tmp.next = next_node
+
+            p = next_node
+
+        # step2. copy random pointer
         p = head
         while p and p.next:
             p.next.random = p.random.next if p.random else None
             p = p.next.next
-        
-        p = head.next
+
+        new_head = head.next
+        p = new_head
         while p and p.next:
             p.next = p.next.next
             p = p.next
         
-        return head.next
+        return new_head
 
+            
+            
 
+        
