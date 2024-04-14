96fa94c8b630e328a9c10dd921d19ab9d06f4301
Wonhee Jang <Qtsky89@gmail.com>
2024-04-14 12:20:57 +0900

    Time: 76 ms (36.17%) | Memory: 20 MB (17.99%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,21 +1,16 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         '''
-        if there are no overlapping, return True
-        else
-            return False
-
-        step1. sort ! by start time
-        step2. if previous interval end time > current intervals start time => overlapping
-               else we can continue
+        step1. sort intervals (start_time, end_time)
+        step2. if collapse it means last end_time > new start_time return False
+               else return True
         '''
 
         intervals.sort()
 
         for i in range(1, len(intervals)):
-            # if previous interval end time > current intervals start time => overlapping
-            # ovelapping
+            # collapse
             if intervals[i-1][1] > intervals[i][0]:
                 return False
-        return True
         
+        return True
