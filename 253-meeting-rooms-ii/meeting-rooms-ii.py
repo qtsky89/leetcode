334348dfbdc2579ea50820049493dea67919f55c
Wonhee Jang <Qtsky89@gmail.com>
2024-04-14 12:16:19 +0900

    Time: 78 ms (35.28%) | Memory: 19.9 MB (56.08%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,23 +1,28 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         '''
-        [0, 30]
-        [5, 10]
-        [15, 20]
+        step1. sort by start time, end time
+        step2. min heap <= endtime
+
+            if min_endtime <  start time => I can reuse the meeting
+            else:
+                increase the meeting room count
+                I put end time to the heap
         '''
 
+        # step1. sort by start time, end time
         intervals.sort()
 
-        end_time_heap = []
-        count = 0
+        # step2. min heap <= start_time
+        heap = []
 
+        meeting_room_count = 0
         for start, end in intervals:
-            # check I can reuse meeting room
-            if end_time_heap and end_time_heap[0] <= start:
-                heapq.heappop(end_time_heap)
-                heapq.heappush(end_time_heap, end)
+            # I don't need new meeting
+            if heap and heap[0] <= start:
+                heapq.heappop(heap)
+                heapq.heappush(heap, end)
             else:
-                count += 1
-                heapq.heappush(end_time_heap, end)
-        return count
-        
+                meeting_room_count += 1
+                heapq.heappush(heap, end)
+        return meeting_room_count
