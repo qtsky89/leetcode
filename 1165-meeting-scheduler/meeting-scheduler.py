4fbdbf3221af1cc7e0d1ece81179367f102cf68e
Wonhee Jang <Qtsky89@gmail.com>
2024-04-15 23:58:26 +0900

    Time: 484 ms (15.14%) | Memory: 22.3 MB (99.43%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.py
+++ 1165-meeting-scheduler/meeting-scheduler.py
@@ -1,20 +1,21 @@
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
-        
         heapq.heapify(slots1)
         heapq.heapify(slots2)
 
         while slots1 and slots2:
-            l1, r1 = slots1[0]
-            l2, r2 = slots2[0]
+            x_start, x_end = slots1[0]
+            y_start, y_end = slots2[0]
 
-            if (min(r1, r2) - max(l1, l2)) >= duration:
-                return [max(l1, l2), max(l1, l2) + duration]
+            tmp_duration = min(x_end, y_end) - max(x_start, y_start)
+
+            if tmp_duration >= duration:
+                return [max(x_start,y_start), max(x_start,y_start) + duration]
             
-            if r1 < r2:
+            if x_end < y_end:
                 heapq.heappop(slots1)
             else:
                 heapq.heappop(slots2)
         return []
 
-            
+        
