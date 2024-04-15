f84c350bcedff3c281c657451010fd80eb147930
Wonhee Jang <Qtsky89@gmail.com>
2024-04-15 17:26:48 +0900

    Time: 58 ms (48.03%) | Memory: 16.6 MB (60.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -12,14 +12,12 @@ class Solution:
         longest_length = 0
         while p2 <= len(s)-1:
             if s[p2] not in visited:
-                visited.add(s[p2])
                 longest_length = max(longest_length, p2-p1+1)
-                p2 += 1
             else:
                 while p1 <= p2 and (s[p2] in visited):
                     visited.remove(s[p1])
                     p1 += 1
-                visited.add(s[p2])
-                p2 += 1
+            visited.add(s[p2])
+            p2 += 1
         
         return longest_length
