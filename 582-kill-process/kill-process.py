fb2b985eca6353c4ee518cc7ae0e6abed9a629d4
Wonhee Jang <Qtsky89@gmail.com>
2024-04-15 15:11:22 +0900

    Time: 305 ms (84.19%) | Memory: 23.1 MB (92.33%) - LeetSync

--- 582-kill-process/kill-process.py
+++ 582-kill-process/kill-process.py
@@ -1,36 +1,49 @@
-from collections import defaultdict
-
 class Solution:
-    def killProcess(self, pids: List[int], ppids: List[int], kill: int) -> List[int]:
+    def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
         '''
-        index pid  ppid (parent)
-        0      1    3
-        1      3    0
-        2     10    5
-        3      5    3
-        
+        pid = [1,3,10,5], ppid = [3,0,5,3], kill = 5
+                    ^                 ^ 
+                            index= 2 
+           pid ppid
+        0   1   3
+        1   3   0
+        2  10   5
+        3   5   3
+
+
         {
-            3 : [1]
             0 : [3]
+            1 : []
+            3 : [1]
+            10 : []
             5 : [10]
-            3 : [5]
         }
         '''
 
-        hash_map = defaultdict(list)
-        for ppid, pid in zip(ppids, pids):
-            hash_map[ppid].append(pid)
+        hash_map = {}
         
+        for p, pp in zip(pid, ppid):
+            if pp in hash_map:
+                hash_map[pp].append(p)
+            else:
+                hash_map[pp] = [p]
+        
+        q = deque([kill])
+
         ret = []
+        while q:
+            item = q.popleft()
+            ret.append(item)
 
-        def dfs(current_pid: int):
-            ret.append(current_pid)
-
-            for new_pid in hash_map[current_pid]:
-                dfs(new_pid)
+            if item in hash_map:
+                for new_item in hash_map[item]:
+                    q.append(new_item)
+        return ret
         
-        dfs(kill)
+        
 
+
+        
         return ret
 
         
