b6c283bdb08f0409a69f0eaffb016f2ea6a518fd
Wonhee Jang <Qtsky89@gmail.com>
2024-04-15 17:09:43 +0900

    Time: 74 ms (5.37%) | Memory: 16.6 MB (74.29%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,24 +1,38 @@
-
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         '''
-        c = {
+        step1.
+        {
             i : 2
-            love : 2
+            love: 2
             leetcode: 1
-            cofing: 1
+            coding: 1
         }
+
+        step2.
+        heap = [[-2, i] [-2, love] [1, leetcode] [1, coding]]
+
+        step3.
+        ret = [i, love, ...]
         '''
+
+        # step1. counter
         c = Counter(words)
 
+        # step2. heap
         heap = []
-        for key in c:
-            heapq.heappush(heap, [-c[key], key])
-
+        for key, value in c.items():
+            heapq.heappush(heap, [-value, key])
         
+        # step3. make ret
         ret = []
         while k > 0:
-            _, w = heappop(heap)
-            ret.append(w)
+            _, key = heapq.heappop(heap)
+            ret.append(key)
             k -= 1
+        
         return ret
+
+        
+
+        
