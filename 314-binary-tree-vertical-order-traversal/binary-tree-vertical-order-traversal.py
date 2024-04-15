004f78a6eea540b17a7df1cf381b90531a6c3884
Wonhee Jang <Qtsky89@gmail.com>
2024-04-15 17:41:15 +0900

    Time: 36 ms (69.22%) | Memory: 16.6 MB (53.29%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -6,44 +6,31 @@
 #         self.right = right
 
 from collections import deque
-
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        '''
-        whenever left, key -= 1
-        whenever right, key += 1
-
-        hash_map = {
-            0 : [3]
-            -1: [9, 5]
-            ...
-        }
-        change hash_map to list
-        '''
+        hash_map = {}
 
         if not root:
             return []
 
-        hash_map = defaultdict(list)
-
-        q = deque([[root, 0]])
+        q  = deque([[root, 0]])
 
         while q:
             item, key = q.popleft()
 
-            hash_map[key].append(item.val)
-
+            if key in hash_map:
+                hash_map[key].append(item.val)
+            else:
+                hash_map[key] = [item.val]
+            
             if item.left:
                 q.append([item.left, key-1])
-            
             if item.right:
                 q.append([item.right, key+1])
         
         ret = []
         for key in sorted(hash_map.keys()):
             ret.append(hash_map[key])
+        
         return ret
-            
-            
-
         
