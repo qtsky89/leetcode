db2623e1119411cb518a61973463cc4286229e50
Wonhee Jang <Qtsky89@gmail.com>
2024-04-16 15:47:49 +0900

    Time: 134 ms (99.09%) | Memory: 17.3 MB (76.67%) - LeetSync

--- 694-number-of-distinct-islands/number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/number-of-distinct-islands.py
@@ -1,29 +1,37 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
-        def dfs(first_i: int, first_j: int, i: int, j: int, shape: list[str]) -> None:
-            # make it water
+        '''
+        N R L U D
+        '''
+
+        def dfs(i: int, j: int, current_work: list[str]) -> None:
+            # don't want to visit again
             grid[i][j] = 0
 
-            if i+1 <= len(grid)-1 and grid[i+1][j] == 1:
-                shape.append('D')
-                dfs(first_i, first_j, i+1, j, shape)
-            if j+1 <= len(grid[0])-1 and grid[i][j+1] == 1:
-                shape.append('R')
-                dfs(first_i, first_j, i, j+1, shape)
-            if i-1 >= 0 and grid[i-1][j] == 1:
-                shape.append('U')
-                dfs(first_i, first_j, i-1, j, shape)
-            if j-1 >= 0 and grid[i][j-1] == 1:
-                shape.append('L')
-                dfs(first_i, first_j, i, j-1, shape)
-            shape.append('X')
+            if i + 1 <= len(grid)-1 and grid[i+1][j] == 1:
+                current_work.append('D')
+                dfs(i+1, j, current_work)
             
-        shapes = set()
+            if i - 1 >= 0 and grid[i-1][j] == 1:
+                current_work.append('U')
+                dfs(i-1, j, current_work)
+            
+            if j + 1 <= len(grid[0]) -1 and grid[i][j+1] == 1:
+                current_work.append('R')
+                dfs(i, j+1, current_work)
+            
+            if j - 1 >= 0 and grid[i][j-1] == 1:
+                current_work.append('L')
+                dfs(i, j-1, current_work)
+            
+            current_work.append('Z')
+
+        ret: set[str] = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    shape = ['N']
-                    dfs(i, j, i, j, shape)
-                    shapes.add(''.join(shape))
-
-        return len(shapes)
+                    current_work = ['N']
+                    dfs(i, j, current_work)
+                    ret.add(''.join(current_work))
+        
+        return len(ret)
