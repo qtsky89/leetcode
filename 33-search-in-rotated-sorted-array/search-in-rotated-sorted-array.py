04da6328201ff0cc8a5b86f249d6068a8db2155a
Wonhee Jang <Qtsky89@gmail.com>
2024-04-16 15:20:23 +0900

    Time: 39 ms (81.31%) | Memory: 16.9 MB (93.65%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,43 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         '''
-        nums = [4, 5, 6, 7, 0, 1, 2] target = 0
-
-
-        1. I need to find pivot index first
-        2. divide into two array group
-        3. search both of them using binary search
-
-        example1. nums = [4, 5, 6, 7, 0, 1, 2] target = 0
-
-        [4, 5, 6, 7] [0, 1, 2]
-                     lmr
-
-        nums[m] => 7                    target = 0   target < nums[m] and target < nums[l]
-        nums[m] = > 1                   target = 0    target < nums[m] check left
+        nums = 4 5 6 7 0 1 2
+               ^     ^     ^   target = 0
         '''
 
         l, r = 0, len(nums)-1
 
         while l <= r:
-            m = (l+r) // 2
+            mid = (l + r) // 2
 
-            if target == nums[m]:
-                return m
-         
+            if nums[mid] == target:
+                return mid
+            
             # left is sorted
-            if nums[l] <= nums[m]:
-                if target > nums[m] or target < nums[l]:
-                    l = m + 1
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid -1
                 else:
-                    r = m - 1
-            else:
-                if target < nums[m] or target > nums[r]:
-                    r = m - 1
+                    l = mid + 1
+            else: # right is sorted
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
                 else:
-                    l = m + 1
+                    r = mid - 1
 
         return -1
-        
-        
-        
