7639a20403f0c13eec1b5d1cadb1cb19a61edb49
Wonhee Jang <Qtsky89@gmail.com>
2024-04-21 22:24:56 +0900

    Time: 135 ms (49.64%) | Memory: 31.5 MB (27.23%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,29 +6,22 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        '''
-        1. make dfs function to travel. but keep the maximum value of the path
-        2. if current_node value is bigger or same the maximum value count += 1
-        3. return count
-        '''
-
-        if not root:
-            return 0
-
-
+        
         count = 0
-        def dfs(node: TreeNode, maximum_value: int):
+
+        def dfs(node: TreeNode, max_value: int):
             if not node:
                 return
             
-            # 2. if current_node value is bigger or same the maximum value count += 1
-            if node.val >= maximum_value:
+            if node.val >= max_value:
                 nonlocal count
                 count += 1
             
-            dfs(node.left, max(node.val, maximum_value))
-            dfs(node.right, max(node.val, maximum_value))
+            max_value = max(max_value, node.val)
 
+            dfs(node.left, max_value)
+            dfs(node.right, max_value)
+            
         dfs(root, root.val)
 
         return count
