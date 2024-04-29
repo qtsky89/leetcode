5f942bcbe83f8f1837917ee463cb5a0e4cd3aab2
Wonhee Jang <Qtsky89@gmail.com>
2024-04-29 23:07:18 +0900

    Time: 51 ms (79.02%) | Memory: 16.7 MB (82.07%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,24 +1,22 @@
 class Solution:
-    def __init__(self):
-        self._cache = {0:0, 1:0}
-
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         '''
-        0   1   2  3
-cost    10  15  20           => 15
-                             => 10 + 20 ...
-                             => 10 + 15 + 20 
-       dp[0] => 0
-       dp[1] => 0
-       dp[2] => min(dp[0] + costs[0], dp[1] + costs[1])
-       dp[3] => min(dp[1] + costs[1], dp[2] + costs[1])
-       ...
-       dp[n] => min(dp[n-2] + costs[n-2], dp[n-1] + costs[n-1])
+        10 + 20 => 30
+        15
+
+        cost = 10 15 20
+        dp   = 10 15 
+
+
+        dp[i] = min(dp[i-2], dp[i-1]) + cost[i]
         '''
 
-        def dp(i : int) -> int:
-            if i not in self._cache:
-                self._cache[i] = min(dp(i-2) + cost[i-2], dp(i-1) + cost[i-1])
-            return self._cache[i]
-            
-        return dp(len(cost))
+        dp = [0] * len(cost)
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i-2], dp[i-1]) + cost[i]
+        
+        return min(dp[-1], dp[-2])
