15404fb6cb89d461c24604b9813c5faf265c2396
Wonhee Jang <Qtsky89@gmail.com>
2024-09-08 23:30:23 +0900

    Time: 264 ms (60.87%) | Memory: 63.7 MB (79.70%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,40 +1,40 @@
+from random import randint
+
 class RandomizedSet:
     def __init__(self):
-        # 2, 3, 4
-        self.list: list[int] = [] # [2, 4, 3]
+        self._dic = {}
+        self._list = []
 
-        # {2: 0, 3: 2, 4: 1}
-        self.dic: dict[int, int] = {}
-        
     def insert(self, val: int) -> bool:
-        if val in self.dic:
+        if val in self._dic:
             return False
         else:
-            self.list.append(val)
-            self.dic[val] = len(self.list)-1
+            self._list.append(val)
+            self._dic[val] = len(self._list)-1
             return True
-            
+
     def remove(self, val: int) -> bool:
-        if val not in self.dic:
+        if val not in self._dic:
             return False
-        else:
-            a_index = self.dic[val]
 
-            self.dic[self.list[-1]] = a_index
-            self.list[a_index], self.list[-1] = self.list[-1], self.list[a_index]
+        index = self._dic[val]
+        last_index = len(self._list)-1
+        last_val = self._list[-1]
+        self._dic[last_val] = index
 
-            del self.dic[val]
-            self.list.pop()
+        self._list[index], self._list[last_index] = self._list[last_index], self._list[index]
+        self._list.pop()
 
-            return True
+        del self._dic[val]
+        
 
+        return True
 
     def getRandom(self) -> int:
-        index = random.randint(0, len(self.list)-1)
+        index = randint(0, len(self._list)-1)
 
-        return self.list[index]
-        
-        
+        return self._list[index]
+
 
 
 # Your RandomizedSet object will be instantiated and called as such:
