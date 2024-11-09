c96af64966d6e03cfcb0537241e9209f70b0616f
Wonhee Jang <Qtsky89@gmail.com>
2024-11-09 16:26:07 +0900

    Time: 11 ms (37.66%) | Memory: 16.9 MB (89.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -19,23 +19,27 @@ class Solution:
         return True'''
 
     def isPalindrome(self, s: str) -> bool:
+        '''
+        s = "Was it a car or a cat I saw?"
+                          ^^
+
+
+        '''
+
         p1, p2 = 0, len(s)-1
-
-        while p1 < p2:
-            a = s[p1].lower()
-            b = s[p2].lower()
-
-            if not a.isalnum():
+        while p1 <= p2:
+            while p1 <= len(s)-1 and not s[p1].isalnum():
                 p1 += 1
-                continue
-            elif not b.isalnum():
+            
+            while p2 >= 0 and not s[p2].isalnum():
                 p2 -= 1
-                continue
-            elif a != b:
+                        
+            if p1<=p2 and s[p1].lower() != s[p2].lower():
                 return False
             
             p1 += 1
             p2 -= 1
+        
         return True
                 
                 
