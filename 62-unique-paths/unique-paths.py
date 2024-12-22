4c27bb19484049e2ce725c992205518279b12f34
Wonhee Jang <Qtsky89@gmail.com>
2024-12-22 16:54:39 +0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (6.90%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -4,7 +4,7 @@ class Solution:
         dp[i][j] = dp[i-1][j] + do[i][j-1]
         '''
 
-        dp = [[1 for __ in range(n)] for _ in range(m)]
+        dp = [[1 for _ in range(n)] for _ in range(m)]
 
         for i in range(1, m):
             for j in range(1, n):
