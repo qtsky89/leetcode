3a7e1d8c2ed0ef8fcb818c72c46b09c4e6ac1519
Wonhee Jang <Qtsky89@gmail.com>
2024-12-23 21:15:56 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (6.12%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,23 +1,31 @@
-from collections import deque
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         '''
         nums = [1, 2, 3]
+
+        []
+        [1]
+        [2]
+        [3]
+        [12]  =>  [21]
+        [23]
+        [13]
+        [123]
         '''
 
-        q = deque([[]])
+        nums.sort()
 
-        ret = []
-        while q:
-            item = q.popleft()
-            
-            ret.append(item)
+        ret: list[list[int]] = []
+
+        # something here
+        def dfs(current_work: list[int]) -> None:
+            ret.append(current_work)
 
             for n in nums:
-                if not item:
-                    q.append(item + [n])
-                elif item[-1] < n:
-                    q.append(item + [n])
+                if not current_work or (current_work and current_work[-1] < n):
+                    dfs(current_work + [n])
+
+        dfs([])
+
         return ret
-                    
+        
