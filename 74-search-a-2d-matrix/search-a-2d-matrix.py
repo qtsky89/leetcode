302462f2d85f0e25646e237c0180ee78ce652f27
Wonhee Jang <Qtsky89@gmail.com>
2024-12-23 14:41:43 +0900

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.93%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,33 +1,37 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        cols = [matrix[i][0]  for i in range(len(matrix))]
+        '''
+        1, 10, 23, 72    target=3
+        tb         
+         ^
 
-        base_row = 0
 
-        i, j = 0, len(matrix)-1
-        while i <= j:
-            mid = (i+j) // 2
+        val <= target
+        '''
 
-            if matrix[mid][0] == target:
-                return True
-            
-            if (matrix[mid][0] < target and mid == len(matrix)-1) or (matrix[mid][0] < target < matrix[mid+1][0]):
-                base_row = mid
-                break
-            
-            if target > matrix[mid][0]:
-                i = mid+1
+        t, b = 0, len(matrix)-1
+
+        while t <= b:
+            mid = (t+b) // 2
+
+            if matrix[mid][0] <= target:
+                t = mid + 1
             else:
-                j = mid-1
+                b = mid - 1
+
+        # what we want    
+        row = (t + b) // 2
+
+        l, r = 0, len(matrix[0])-1
+
+        while l <= r:
+            mid = (l+r) // 2
+
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] > target:
+                r = mid-1
+            else:
+                l = mid + 1
         
-        i, j = 0, len(matrix[0])-1
-        while i <= j:
-            mid = (i + j) // 2
-
-            if matrix[base_row][mid] == target:
-                return True
-            elif target > matrix[base_row][mid]:
-                i = mid + 1
-            else:
-                j = mid -1
         return False
