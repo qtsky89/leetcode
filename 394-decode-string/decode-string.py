7c72419714ed747d36b7d40dd967233054bf30ef
Wonhee Jang <Qtsky89@gmail.com>
2024-12-24 15:42:00 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (5.87%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,53 +1,47 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        '''
-        k[encoded_string]
-
-        example1.
-            s = 3[a]2[bc]
-            ret = aaabcbc
-        
-        example2.
-            s = 13 [ a 2 [ c ] ]
-                               ^
-            
-            stack = accaccacc...
-                'acc' * 13
-        example3.
-            s = 2[abc]3[cd]ef
-            ret = abcabccdcdcdef
-        '''
-
         stack = []
 
-        i = 0
+        '''
+            23[abc]
+            i
+              j
+        '''
+        i=0
         while i <= len(s)-1:
-            # handle digit
             if s[i].isdigit():
-                tmp = ''
-                while s[i] != '[':
-                    tmp += s[i]
-                    i += 1
-                stack.append(int(tmp))
+                j = i
+                while j <= len(s)-1 and s[j].isdigit():
+                    j += 1
+                stack.append(int(s[i:j]))
+
+                i = j
                 continue
-            
-            if s[i] == ']':
+            elif s[i] == '[':
+                stack.append(s[i])
+                i += 1
+            elif s[i].isalpha():
+                stack.append(s[i])
+                i += 1
+            elif s[i] == ']':
                 tmp = ''
-                
                 while stack and stack[-1] != '[':
                     tmp = stack.pop() + tmp
                 
-                stack.pop() # remove [
+                '''
+                tmp = abc
+                '''
 
-                tmp = tmp * stack.pop() # get number
-                stack.append(tmp)
-            else:
-                stack.append(s[i])
-            
-            i+=1
-        return ''.join(stack)
-            
+                # remove open bracket
+                stack.pop()
+
+                # 23, 2, 3
+                times = stack.pop()
                 
+                stack.append(tmp * times)
+                i += 1
+        
+        return ''.join(stack)
 
 
-
+                
