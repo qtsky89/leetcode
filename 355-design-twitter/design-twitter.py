4e15f2c2ad9fce38b52f0bebb556c3afda08cc5d
Wonhee Jang <Qtsky89@gmail.com>
2024-12-24 20:08:47 +0900

    Time: 15 ms (24.88%) | Memory: 27.8 MB (6.54%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -1,59 +1,59 @@
+'''
+self._follow_map = {
+    1 : {1, 2, 3, 4}
+            ^  ^  ^
+    }
+self._tweetMap = {
+    1 : [1, 2, 3]
+               ^
+}
+'''
+
+
 class Twitter:
-    def __init__(self):
-        '''
-        data_map = {
-            user_id: [[timeline, tweet_id]]
-        }
-        '''
-        self.data_map = {}
-    
-        '''
-        follow_map = {
-            follower_id = {flowee_id1, flowee_id2 ...}
-        }
-        '''
-        self.follow_map = {}
-        self.time = 0
-        
-    def postTweet(self, user_id: int, tweet_id: int) -> None:
-        if user_id in self.data_map:
-            self.data_map[user_id].append([-self.time, tweet_id])
+    def __init__(self) -> None:
+        self._follow_map: dict[int, set[int]] = {}
+        self._tweet_map: dict[int, tuple[int, int]] = {}
+        self._time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self._time += 1
+        if userId in self._tweet_map:
+            self._tweet_map[userId].append((self._time, tweetId))
         else:
-            self.data_map[user_id] = [[-self.time, tweet_id]]
-        self.time += 1
-
-    def getNewsFeed(self, user_id: int) -> List[int]:
-        followee_set = set()
-        if user_id in self.follow_map:
-            followee_set = self.follow_map[user_id]
+            self._tweet_map[userId] = [(self._time, tweetId)]
         
-        # include my self
-        followee_set.add(user_id)
+    def getNewsFeed(self, userId: int) -> List[int]:
+        # folloeeIds
+        followeeIds = [userId]
+        if userId in self._follow_map:
+            followeeIds.extend(list(self._follow_map[userId]))
 
-        heap = []
-
-        for folowee_id in followee_set:
-            if folowee_id in self.data_map:
-                for item in self.data_map[folowee_id]:
-                    heapq.heappush(heap, item)
+        all_tweets: list[tuple[int, int]] = []
+        for user in followeeIds:
+            if user in self._tweet_map:
+                all_tweets.extend(self._tweet_map[user])
         
+        all_tweets.sort(key=lambda x: x[0], reverse=True)
+
         ret = []
-        for _ in range(min(len(heap), 10)):
-            timeline, tweet_id = heapq.heappop(heap)
-            ret.append(tweet_id)
-        
+        for i in range(min(10, len(all_tweets))):
+            ret.append(all_tweets[i][1])
         return ret
+
+
         
-
-    def follow(self, follower_id: int, followee_id: int) -> None:
-        if follower_id in self.follow_map:
-            self.follow_map[follower_id].add(followee_id)
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self._follow_map:
+            self._follow_map[followerId].add(followeeId)
         else:
-            self.follow_map[follower_id] = {followee_id}
+            self._follow_map[followerId] = {followeeId}
 
-    def unfollow(self, follower_id: int, followee_id: int) -> None:
-        if follower_id in self.follow_map:
-            self.follow_map[follower_id].remove(followee_id)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self._follow_map:
+            self._follow_map[followerId].remove(followeeId)
+
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
