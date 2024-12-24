c73e0125ad08b1268009eb724dc91adca3e20bc3
Wonhee Jang <Qtsky89@gmail.com>
2024-12-24 14:54:29 +0900

    Time: 3 ms (88.55%) | Memory: 18.9 MB (5.07%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,37 +1,21 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         '''
-        pivot index
-        nums =      [ 1, 7, 3, 6, 5, 6  ]
-        left_sum =  [ 0  1  8  11 17 22 ]                  
-     right_sum =    [ 27 20 17 11 6  0 ]
-                 ^                 0, 27
-                    ^              1, 20
-                       ^           8, 17
-                          ^        11  11 => return 3
-                                   17  6
-                                   22  0
+        nums = 1 7 3 6 5 6 
+                              left: 0, right: 28
+               ^              left: 0, right: 27
+                 ^            left: 1, right: 20
+                   ^          left: 8, right: 17
+                     ^        left: 11, right: 11,  return 3
         '''
 
-        left_sum = [0] * len(nums)
-        right_sum = [0] * len(nums)
-
-        left_sum[0] = nums[0]
-        for i in range(1, len(nums)):
-            left_sum[i] = left_sum[i-1] + nums[i]
-        
-        left_sum.insert(0, 0)
-        left_sum.pop()
-        
-        right_sum[-1] = nums[-1]
-        for i in range(len(nums)-2, -1, -1):
-            right_sum[i] = right_sum[i+1] + nums[i]
-        
-        right_sum.pop(0)
-        right_sum.append(0)
+        left_sum = 0
+        right_sum = sum(nums)
 
         for i in range(len(nums)):
-            if left_sum[i] == right_sum[i]:
+            left_sum += nums[i-1] if i-1 >=0 else 0
+            right_sum -= nums[i]
+
+            if left_sum == right_sum:
                 return i
         return -1
-        
