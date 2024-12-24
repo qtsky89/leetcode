bcb7be39d1a98b6cf7c00c4a7ecef392277fe30f
Wonhee Jang <Qtsky89@gmail.com>
2024-12-24 16:36:31 +0900

    Time: 11 ms (18.14%) | Memory: 18.8 MB (24.13%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,57 +1,59 @@
+from collections import deque
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         '''
-        {
-            0 : [1, 3]
-            1 : [2]
-            3 : [1]
+        numCourses = 5, prerequisites = [1, 0] [4, 0] [3, 4] [3, 2]
+
+        make adjacent_map
+        adjacent_map = {
+            0 : [1, 4]
+            2 : [3]
+            4 : [3]
         }
 
-        count = [0, 0, 0, 0]
-                 0  2  1  1
+        count_map = {
+            0 : 0
+            1 : 1
+            2 : 0
+            3 : 1
+            4 : 1
+        }
+
+        always go through when count is zero
+
+        if we could visit all courses, return True else return False
         '''
-        
-        count = [0] * numCourses
-        # make adjacent_map
-        adjacent_map = {}
+
+        adjacent_map: dict[int, list[int]] = {}
+        count_map: dict[int, int] = {i: 0 for i in range(numCourses)}
+
         for end, start in prerequisites:
             if start in adjacent_map:
                 adjacent_map[start].append(end)
             else:
                 adjacent_map[start] = [end]
             
-            count[end] += 1
-
-        q = deque()
+            count_map[end] += 1
+        
         visited = set()
 
-        for i in range(len(count)):
-            if count[i] == 0:
-                q.append(i)
-        
+        q = deque()
+        for key, count in count_map.items():
+            if count == 0:
+                q.append(key)
+
+        # 0, 2
         while q:
-            item = q.popleft()
-
-            visited.add(item)
-
-            if item in adjacent_map:
-                for new_item in adjacent_map[item]:
-                    if new_item not in visited:
-                        count[new_item] -= 1
-                        if count[new_item] == 0:
-                            q.append(new_item)
-        
-        return len(visited) == numCourses
-                
-        
-        
-        
+            # 0
+            current_course = q.popleft()
+            visited.add(current_course)
             
-        
+            if current_course in adjacent_map:
+                for next_course in adjacent_map[current_course]:
+                    if next_course not in visited:
+                        count_map[next_course] -= 1
+                        if count_map[next_course] == 0:
+                            q.append(next_course)
 
-
-        
-        
-
-
-
+        return len(visited) == numCourses
