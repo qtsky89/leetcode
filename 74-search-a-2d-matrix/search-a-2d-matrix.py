666a41b332464a903722c49a930501eb76936cc9
Wonhee Jang <Qtsky89@gmail.com>
2024-12-25 17:57:52 +0900

    Time: 0 ms (100.00%) | Memory: 18.4 MB (6.77%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,14 +1,5 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        '''
-        1, 10, 23, 72    target=3
-        tb         
-         ^
-
-
-        val <= target
-        '''
-
         t, b = 0, len(matrix)-1
 
         while t <= b:
@@ -17,11 +8,10 @@ class Solution:
             if matrix[mid][0] <= target:
                 t = mid + 1
             else:
-                b = mid - 1
-
-        # what we want    
-        row = (t + b) // 2
-
+                b = mid -1
+            
+        row = (t+b) // 2
+        
         l, r = 0, len(matrix[0])-1
 
         while l <= r:
@@ -29,9 +19,25 @@ class Solution:
 
             if matrix[row][mid] == target:
                 return True
-            elif matrix[row][mid] > target:
-                r = mid-1
-            else:
+            elif matrix[row][mid] < target:
                 l = mid + 1
-        
+            else:
+                r = mid -1
         return False
+                
+            
+
+
+        
+        
+        
+        '''
+        1 10 23  44       target=3
+        t m       b
+        tb
+
+        
+
+        
+        '''
+        
