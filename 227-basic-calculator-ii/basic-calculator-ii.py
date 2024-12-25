daeabc85563773d880ddd5e9bb05e1a035eda3ab
Wonhee Jang <Qtsky89@gmail.com>
2024-12-25 19:25:59 +0900

    Time: 147 ms (7.93%) | Memory: 21.6 MB (15.45%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,39 +1,56 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        '''
+        example1.
+            s = 3 + 2 * 2
+                        ^
+            stack = 3, 4
+
+            sum(stack) = 7
+
+        example2.
+            s = 3 / 2
+
+            stack = 1
+
+            sum(stack) = 1
+        
+        example3.
+            s = 3 - 5 / 2
+
+            stack = 3, -2
+
+            sum(stack) = 1
+        '''
+
         i = 0
-
         stack = []
-
-        is_minus = False
         while i <= len(s)-1:
-            if s[i] == " " or s[i] == '+':
-                i+=1
+            if s[i] == ' ' or s[i] == '+':
+                i += 1
+                continue
+
+            if s[i] in '-*/':
+                stack.append(s[i])
+                i += 1
             elif s[i].isdigit():
-                tmp = ''
                 j = i
                 while j <= len(s)-1 and s[j].isdigit():
                     j += 1
-                
-                if is_minus:
-                    tmp = '-' + s[i:j]
-                    is_minus = False
-                else:
-                    tmp = s[i:j]
-                i = j
+                tmp_num = int(s[i:j])
 
-                if stack and stack[-1] == '*':
+                if stack and stack[-1] == '-':
                     stack.pop()
-                    stack.append(int(stack.pop()) * int(tmp))
+                    stack.append(-tmp_num)
+                elif stack and stack[-1] == '*':
+                    stack.pop()
+                    stack.append(stack.pop() * tmp_num)
                 elif stack and stack[-1] == '/':
                     stack.pop()
-                    stack.append(int(int(stack.pop()) / int(tmp)))
+                    stack.append(int(stack.pop() / tmp_num))
                 else:
-                    stack.append(int(tmp))
-            elif s[i] == '-':
-                is_minus = True
-                i += 1
-            elif s[i] in '*/':
-                stack.append(s[i])
-                i += 1
-
+                    stack.append(tmp_num)
+                i = j
+        
         return sum(stack)
+
