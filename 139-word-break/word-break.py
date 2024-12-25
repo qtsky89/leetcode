b617c794bef6feddb87d76aa67056240727190b8
Wonhee Jang <Qtsky89@gmail.com>
2024-12-25 12:47:45 +0900

    Time: 4 ms (40.70%) | Memory: 18.1 MB (6.38%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,23 +1,28 @@
 class Solution:
+    '''
+    s = "leetcode"
+                  ^
+
+    '''
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         ret = False
-
         visited = set()
-        def dfs(remaining: str) -> None:
-            if remaining in visited:
+        def dfs(current_i: int) -> None:
+            nonlocal ret
+            if ret:
                 return
-            else:
-                visited.add(remaining)
 
-            if remaining == '':
-                nonlocal ret
+            if current_i == len(s):
                 ret = True
                 return
             
+            visited.add(current_i)
+            
+            # word = leet
             for word in wordDict:
-                if word == remaining[:len(word)]:
-                    dfs(remaining[len(word):])
-        dfs(s)
+                if s[current_i:current_i+len(word)] == word and current_i+len(word) not in visited:
+                    dfs(current_i+len(word))
+
+        dfs(0)
 
         return ret
-        
