d0766ff4c3f625df51fe75e4ff690fef8daf2a58
Wonhee Jang <Qtsky89@gmail.com>
2024-12-26 14:55:00 +0900

    Time: 32 ms (65.73%) | Memory: 24.8 MB (8.01%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -1,42 +1,54 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         '''
-        ajacent_map = {
-            1 : [2, 3]
-            2 : [1, 4]
-            3 : [1]
-            4 : [2]
+        adjacent_map = {
+            1: [2, 3]
+            2: [1, 4]
+            3: [1]
+            4: [2]          
         }
 
+        # blue => 1, red => -1
+        colors_map = {
+            1 : 0
+            2 : 0
+            3 : 0
+            4 : 0
+        }
+
+        if already has color and that color doesn't match:
+            return False
+        else
+            return True
         '''
 
-        ajacent_map: defaultdict[int, list[int]] = defaultdict(list)
-
+        adjacent_map = defaultdict(list)
         for a, b in dislikes:
-            ajacent_map[a].append(b)
-            ajacent_map[b].append(a)
-
-        colors = [0] * (n+1)
+            adjacent_map[a].append(b)
+            adjacent_map[b].append(a)
+        
+        colors_map = defaultdict(int)
 
         ret = True
-        def dfs(i: int, color: int) -> None:
+        # current_color => 1(BLUE), -1(RED) 0(WHITE)
+        def dfs(current_people: int, current_color: int) -> None:
             nonlocal ret
+
             if not ret:
                 return
-            colors[i] = color
 
-            for neighbor in ajacent_map[i]:
-                if colors[neighbor] == colors[i]:
+            colors_map[current_people] = current_color
+
+            for next_people in adjacent_map[current_people]:
+                if colors_map[next_people] == current_color:
                     ret = False
-                    return
-
-                if colors[neighbor] == 0:
-                    dfs(neighbor, -1*colors[i])
-        
-        for i in range(n):
-            if not colors[i]:
-                dfs(i, 1)
+                if colors_map[next_people] == 0:
+                    dfs(next_people, current_color * (-1))
+                
+        for current_people in range(1, n):
+            if colors_map[current_people] == 0:
+                dfs(current_people, 1)
         
         return ret
