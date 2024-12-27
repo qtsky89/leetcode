80dfd5c00cffbbb41bac3298a0769ee8a567aafa
Wonhee Jang <Qtsky89@gmail.com>
2024-12-27 23:14:16 +0900

    Time: 0 ms (100.00%) | Memory: 18.4 MB (5.70%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,5 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        '''
+         
+          1 3 5 7 
+        ->10 11 16 20   target = 16
+        23 30 34 60
+        
+        target
+
+        first  search through first column => [1, 10, 23]
+            find equal or less than target
+
+
+        second search through that row [1, 3, 5, 7]
+        '''
+
         t, b = 0, len(matrix)-1
 
         while t <= b:
@@ -8,12 +23,11 @@ class Solution:
             if matrix[mid][0] <= target:
                 t = mid + 1
             else:
-                b = mid -1
-            
-        row = b
-        
-        l, r = 0, len(matrix[0])-1
+                b = mid - 1
 
+        row = (t+b) // 2
+
+        l, r = 0, len(matrix[0])-1
         while l <= r:
             mid = (l+r) // 2
 
@@ -22,22 +36,5 @@ class Solution:
             elif matrix[row][mid] < target:
                 l = mid + 1
             else:
-                r = mid -1
+                r = mid - 1
         return False
-                
-            
-
-
-        
-        
-        
-        '''
-        1 10 23  44       target=3
-        t m       b
-        tb
-
-        
-
-        
-        '''
-        
