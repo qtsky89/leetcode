de64fc5656878fe3d6aae797dd15e12be0816347
Wonhee Jang <Qtsky89@gmail.com>
2024-12-28 00:01:02 +0900

    Time: 163 ms (37.70%) | Memory: 19.3 MB (6.40%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,32 +1,35 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         '''
-        piles = [3, 6, 7, 11] h = 8
-                 3  6  7  11  k = 1, fail, 27 hours             
-                 2  3  4   6  k = 2, fail, 15 hours
-                 1  2   3  4  k = 3, fail, 10 hours
-                 1, 2, 2, 3   k = 4  success, 8 hours
+        piles = 3 6 7 11   h=8
+                ^
+      l k=1     3 6 7 11 => 27
+        k=2     2 3 4 6  => 15
+        k=3     1 2 3 4  => 10
+        k=4     1 2 2 3  => 8          k=4
+
+        k=6     1 1 2 2 => 6
+
+     r  k=11    1 1 1 1  => 4
+
+        mid => 6
         '''
 
         l, r = 1, max(piles)
-        # 1 ... 11 find min k
-
         min_k = float('inf')
-        while l <= r:
-            mid = (l + r) // 2
 
-            total = 0
+        while l<=r:
+            print(l, r)
+            mid = (l+r) // 2
+
+            tmp = 0
             for pile in piles:
-                total += math.ceil(pile / mid)
-
-            # one of right answer
-            if total <= h:
-                r = mid - 1
+                tmp += math.ceil(pile/mid)
+            
+            if tmp <= h:
                 min_k = min(min_k, mid)
+                r = mid - 1
             else:
                 l = mid + 1
-
-        return min_k
         
-
-            
+        return min_k
