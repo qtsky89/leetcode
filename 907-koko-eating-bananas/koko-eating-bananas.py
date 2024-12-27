e16d8127517336801602d45c698310283f9c64f8
Wonhee Jang <Qtsky89@gmail.com>
2024-12-28 00:38:09 +0900

    Time: 156 ms (48.38%) | Memory: 18.8 MB (11.86%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,20 +3,19 @@ class Solution:
         '''
         piles = 3 6 7 11   h=8
                 ^
-      l k=1     3 6 7 11 => 27
+        k=1     3 6 7 11 => 27
         k=2     2 3 4 6  => 15
-        k=3     1 2 3 4  => 10
-        k=4     1 2 2 3  => 8          k=4
-
+     r   k=3     1 2 3 4  => 10
+     l mid  k=4     1 2 2 3  => 8          k=4
+       k=5     1 2 2 3  => 8
         k=6     1 1 2 2 => 6
 
-     r  k=11    1 1 1 1  => 4
+       k=11    1 1 1 1  => 4
 
         mid => 6
         '''
 
         l, r = 1, max(piles)
-        min_k = float('inf')
 
         while l<=r:
             mid = (l+r) // 2
@@ -26,9 +25,8 @@ class Solution:
                 tmp += math.ceil(pile/mid)
             
             if tmp <= h:
-                min_k = min(min_k, mid)
                 r = mid - 1
             else:
                 l = mid + 1
         
-        return min_k
+        return l
