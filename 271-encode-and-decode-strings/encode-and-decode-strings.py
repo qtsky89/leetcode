2d54a090f0b1532805a596cd1c4456c9829119a9
Wonhee Jang <Qtsky89@gmail.com>
2024-12-27 15:29:11 +0900

    Time: 75 ms (29.69%) | Memory: 18 MB (13.16%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -2,12 +2,51 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        return 'π'.join(strs)
+
+        '''
+        encoded_str = 5*Hello5*World
+        '''
+
+        ret: list[str] = []
+
+        for s in strs:
+            ret.append(f'{len(s)}*{s}')
+        
+        return ''.join(ret)
+
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        return s.split('π')
+
+        '''
+        encoded_str = '5 * H e l l o 5 * W o r l d'
+                      i  j     
+
+        => ['Hello', 'World']
+        
+        '''
+
+        if s == '':
+            return []
+
+        ret = []
+        i = 0
+        while i <= len(s)-1:
+            j = i
+            while j <= len(s)-1 and s[j] != '*':
+                j += 1
+            length = int(s[i:j])
+
+            ret.append(s[j+1:j+1+length])
+
+            i = (j+1+length)
+        
+        return ret
+                
+
+
+
         
 
 
