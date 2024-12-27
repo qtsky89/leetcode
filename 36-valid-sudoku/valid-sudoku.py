f6714bf4be2b139273131a5675e0193455eb7b14
Wonhee Jang <Qtsky89@gmail.com>
2024-12-27 15:58:02 +0900

    Time: 3 ms (83.42%) | Memory: 17.8 MB (10.43%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,39 +1,40 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # row check
-        for i in range(9):
+        # check every row
+        for i in range(len(board)):
             visited = set()
-            for j in range(9):
+
+            for j in range(len(board[0])):
+                if board[i][j] == '.':
+                    continue
+                
                 if board[i][j] in visited:
                     return False
-                elif board[i][j] != '.':
-                    visited.add(board[i][j])
-        
-        # colum check
-        for j in range(9):
+                visited.add(board[i][j])
+
+        # check every column
+        for j in range(len(board[0])):
             visited = set()
-            for i in range(9):
+
+            for i in range(len(board)):
+                if board[i][j] == '.':
+                    continue
+                
                 if board[i][j] in visited:
                     return False
-                elif board[i][j] != '.':
-                    visited.add(board[i][j])
+                visited.add(board[i][j])
         
-        # sub cell check
-        hash_map = { (i, j) : set() for j in range(3) for i in range(3)}
+        # check sub board
+        for i in range(3):
+            for j in range(3):
+                visited = set()
 
-        for i in range(9):
-            for j in range(9):
-                x = i // 3
-                y = j // 3
-
-                visited = hash_map[(x, y)]
-
-                if board[i][j] in visited:
-                    return False
-                elif board[i][j] != '.':
-                    visited.add(board[i][j])
+                for k in range(3):
+                    for l in range(3):
+                        if board[3*i+k][3*j+l] == '.':
+                            continue
+                        
+                        if board[3*i+k][3*j+l] in visited:
+                            return False
+                        visited.add(board[3*i+k][3*j+l])
         return True
-        
-        
-
-        
