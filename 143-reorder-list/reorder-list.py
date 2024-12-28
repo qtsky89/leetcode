b02fca4c3cf278db63cc08235f54638aa8de2a22
Wonhee Jang <Qtsky89@gmail.com>
2024-12-28 14:54:13 +0900

    Time: 4 ms (38.73%) | Memory: 23.4 MB (6.89%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -10,69 +10,59 @@ class Solution:
         """
         
         '''
-        1 -> 2 <- 3 <-  4
-        s    f
-             s           f
-            
-        1 -> 4 -> 2 -> 3
+        
+
+        1 -> 2 -> 3 -> 4 -> 5-> None  None<- 6 <- 7 <- 8 <- 9 <- 10
+        h    s                                        f
+
+        1 -> 2 -> 10 -> 3 -> 8 ->  4 -> 7 -> 5 -> 6                   
 
 
-        1. find the middle node
-        2. reverse the right nodes
-        3. merge
-        4. return head
+        1 -> 10 -> 2 -> 9 -> 3 -> 8 -> 4 -> 7 -> 5 -> 6
         '''
 
-        ret = ListNode(None, None)
-        ret_p = ret # for adding the new node
 
-        # 1. find the middle node
-        s, f = head, head.next
+        '''
+        1 -> 2-> 3-> 4 -> 5 -> None, None<-6 <- 7 <-  8 <- 9 <- 10
+        p1                                                      p2
+
+        1 -> 10 -> 2
+
+
+        1 -> 2 -> 3
+        s    f
+        '''
+
+
+        s,f  = head, head.next
+
         while f and f.next:
             s, f = s.next, f.next.next
-        
-        # 2. reverse the right nodes
-        # 1<-2->3->4
-        second = s.next
-        s.next = None
 
-        prev, p = None, second
+        
+        tmp = s.next
+        s.next = None
+        
+        prev, p = None, tmp
+
         while p:
             tmp = p.next
             p.next = prev
             prev, p = p, tmp
+
         
-        # 3. merge
         p1, p2 = head, prev
 
         while p1 and p2:
-            tmp1, tmp2 = p1.next, p2.next
-
-            ret_p.next = p1
-            ret_p = ret_p.next
-
-            ret_p.next = p2
-            ret_p = ret_p.next
-
-            p1, p2 = tmp1, tmp2
-
-        while p1:
             tmp1 = p1.next
-            ret_p.next = p1
-            ret_p = ret_p.next
+            tmp2 = p2.next
 
-            p1 = tmp1
+            p1.next, p2.next = p2, p1.next
 
-        return ret_p.next
-
-
-            
-
+            p1,p2 = tmp1, tmp2
+        
         
 
         
-
-
-
-
-        return ret.next
+        
+        
