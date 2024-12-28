11b92dcdccd2890e4babb2405535230fbeb6aa13
Wonhee Jang <Qtsky89@gmail.com>
2024-12-28 21:03:27 +0900

    Time: 8 ms (72.76%) | Memory: 17.8 MB (10.36%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,34 +1,20 @@
-from collections import deque
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
         candidates.sort()
 
-        inputs = []
-
-        for i in range(len(candidates)):
-            inputs.append([[candidates[i]], candidates[i]])
-        
-        # [[[2], 2], [[3], 3], [[6], 6], [[7], 7]]
-
-        q = deque(inputs)
-
-        ret = []
-        while q:
-            # item = [2]
-            item, total = q.popleft()
-
-            if total == target:
-                ret.append(item)
-                continue
-            elif total > target:
-                continue
+        # [2, 3, 6, 7]
+        ret: list[list[int]] = []
+        def dfs(current_work: list[int], current_sum: int, current_index: int) -> None:
+            if current_sum == target:
+                ret.append(current_work)
+                return
             
-            for i in range(len(candidates)):
-                if candidates[i] >= item[-1]:
-                    if total + candidates[i] <= target:
-                        q.append([ [*item, candidates[i]], total + candidates[i] ])
+            for i in range(current_index, len(candidates)):
+                if current_sum + candidates[i] <= target:
+                    dfs(current_work + [candidates[i]], current_sum + candidates[i], i)
+                
+        dfs([], 0, 0)
+
+
         return ret
-            
-            
+        
