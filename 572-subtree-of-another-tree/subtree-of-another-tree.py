ced501a0abff2ef69555bc6e452f30f007586635
Wonhee Jang <Qtsky89@gmail.com>
2024-12-28 17:05:44 +0900

    Time: 39 ms (81.49%) | Memory: 18 MB (13.22%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -4,28 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+def is_identical(node1: TreeNode, node2: TreeNode) -> bool:
+    if not node1 and not node2:
+        return True
+    elif not (node1 and node2):
+        return False
+    elif node1.val != node2.val:
+        return False
+    
+    return is_identical(node1.left, node2.left) and is_identical(node1.right, node2.right)
+
+
 class Solution:
-    def is_identical(self, root: TreeNode, subRoot: TreeNode) -> bool:
-        if not root and subRoot:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:           
+        if not root:
             return False
-        elif root and not subRoot:
-            return False
-        elif not root and not subRoot:
-            return True
-        
-        return root.val == subRoot.val and self.is_identical(root.left, subRoot.left) and self.is_identical(root.right, subRoot.right)
 
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and subRoot:
-            return False
-        elif root and not subRoot:
-            return False
-        elif not root and not subRoot:
+        if (root and subRoot) and root.val == subRoot.val and is_identical(root, subRoot):
             return True
-        
-        if root.val == subRoot.val:
-            if self.is_identical(root, subRoot):
-                return True
-        
+            
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
