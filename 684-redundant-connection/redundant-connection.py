a6de5543a700630768602257e54c34517c947bfb
Wonhee Jang <Qtsky89@gmail.com>
2024-12-29 17:14:17 +0900

    Time: 5 ms (28.38%) | Memory: 18.2 MB (19.09%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,50 +1,56 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         '''
-        # initial state
-        parent = [0, 1, 2, 3]
-        rank =   [0, 1, 1, 1]
+        rank   : [0, 1, 1, 1]
+            index    1  2  3
+        parent : [0, 1, 2, 3]
+            index    1  2  3
 
-        # [1, 2]
-        parent = [0, 1, 1, 3]
-        rank =   [0, 2, 1, 1]
+        [1, 2]
+        rank   : [0, 2, 1, 1]
+            index    1  2  3
+        parent : [0, 1, 1, 3]
+            index    1  2  3
 
-        # [1, 3]
-        parent = [0, 1, 1, 1]
-        rank =   [0, 3, 1, 1]
+        [1, 3]
+        rank   : [0, 3, 1, 1]
+            index    1  2  3
+        parent : [0, 1, 1, 1]
+            index    1  2  3
 
-        # [2, 3]
-        parent = [0, 1, 1, 1]
-        rank =   [0, 3, 1, 1]
+        [2, 3]
+        rank   : [0, 3, 1, 1]
+            index    1  2  3
+        parent : [0, 1, 1, 1]
+            index    1  2  3
         '''
-        '''
-        # initial state
-        parent = [0, 1, 2, 3]
-        rank =   [0, 1, 1, 1]
-        '''
+
+        rank = [1] * (len(edges)+1)
         parent = [i for i in range(len(edges)+1)]
-        rank = [1 for i in range(len(edges)+1)]
 
+        # return parent
         def find(current: int) -> int:
-            while current != parent[current]:
+            while parent[current] != current:
                 current = parent[current]
             return current
-
+        
         def union(x: int, y: int) -> bool:
             p1, p2 = find(x), find(y)
 
-            # it'a already connected
             if p1 == p2:
                 return False
-            
-            if rank[p1] > rank[p2]:
+
+            if rank[p1] >= rank[p2]:
                 rank[p1] += rank[p2]
                 parent[p2] = p1
             else:
                 rank[p2] += rank[p1]
                 parent[p1] = p2
             return True
-        
+
         for x, y in edges:
-            if not union(x, y):
+            ret = union(x, y)
+
+            if not ret:
                 return [x, y]
+        
