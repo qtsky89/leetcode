c2cff5e0c39bad3b7781a9f458346f1b4066ad94
Wonhee Jang <Qtsky89@gmail.com>
2024-12-29 15:11:48 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (6.48%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,38 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        '''
-        []
-        [1]
-        [2]
-        [1, 2]
-        [2, 2]
-        [1, 2, 2]
-        '''
-
         nums.sort()
 
         ret: list[list[int]] = []
         visited = set()
 
-        q = deque([[-1, []]])
+        def dfs(current_work: list[int], current_i: int) -> None:
+            ret.append(current_work)
+            visited.add(tuple(current_work))
 
-        while q:
-            index, item = q.popleft()
-            tmp_tuple = tuple(item)
-            if tmp_tuple in visited:
-                continue
-            else:
-                visited.add(tmp_tuple)
+            for i in range(current_i, len(nums)):
+                tmp = current_work[:] + [nums[i]]
+                if tuple(tmp) not in visited:
+                    dfs(tmp, i+1)
 
-            ret.append(item)
+        dfs([], 0)
 
-            for i in  range(index+1, len(nums)):
-                q.append([i, (*item, nums[i])])
-        
-        return list(ret)
-            
-
-            
-
-        
-
+        return ret
