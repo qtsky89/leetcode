432b7242253787149053d6f56c4805c3e5a8a657
Wonhee Jang <Qtsky89@gmail.com>
2024-12-31 02:05:44 +0900

    Time: 63 ms (18.14%) | Memory: 34.5 MB (17.79%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,40 +1,31 @@
-def is_palindrome(s: str) -> bool:
-    l, r = 0, len(s)-1
+def is_palindrom(s: str) -> bool:
+    p1, p2 = 0, len(s)-1
 
-    while l <= r:
-        if s[l] != s[r]:
+    while p1 <= p2:
+        if s[p1] != s[p2]:
             return False
-        
-        l, r = l+1, r-1
+        p1, p2 = p1+1, p2-1
+    
     return True
     
 
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        '''
-        s = a a b
-            c   j
-
-        [a a b]
-        [aa b]
-
-
-        '''
-
+        
         ret: list[list[str]] = []
 
-        def dfs(current_work: list[str], current_index: int) -> None:
-            if current_index == len(s):
+        def dfs(current_work: list[str], current_i: int) -> None:
+            if current_i == len(s):
                 ret.append(current_work)
                 return
-
-            for j in range(current_index, len(s)):
-                new_char = s[current_index:j+1]
-                if is_palindrome(new_char):
-                    dfs(current_work[:] + [new_char], j+1)
-
+            
+            for j in range(current_i, len(s)):
+                if is_palindrom(s[current_i:j+1]):
+                    dfs(current_work[:] + [s[current_i:j+1]], j+1)
+        
         dfs([], 0)
 
         return ret
+        
 
+        
