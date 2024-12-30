f3f2ed723823b4c2f852574dba97426ec8a6354c
Wonhee Jang <Qtsky89@gmail.com>
2024-12-30 20:27:55 +0900

    Time: 0 ms (100.00%) | Memory: 25 MB (9.45%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,17 +1,20 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        
         '''
-        1 1 2 3 3 4 4 8 8
-                ^
-        0 1 2 3 4 4 5 5 6 6
+               0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16  17  18
+                                          ^
 
-        mid = 4 mid+1 =5 
+        nums = 1, 1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10
+               l                          m                            
+               l           m           r
+               l  m  r
+                    lmr
 
-        nums[mid] == nums[mid+1] the target should be in the right
-        nums[mid] != nums[mid+1] the target should be in the left
+        1. m is single element or not?
 
-        if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1] => target
+        2. left length is odd go left
+
+        3. right length is odd go right
         '''
 
         if len(nums) == 1:
@@ -19,27 +22,20 @@ class Solution:
 
         l, r = 0, len(nums)-1
 
-        while l<=r:
-            mid = (l+r) // 2
+        while l <= r:
+            m = l + (r-l)//2
 
-            # target
-            if (mid==0 or nums[mid] != nums[mid-1]) and (mid==len(nums)-1 or nums[mid] != nums[mid+1]):
-                return nums[mid]
+            # TODO. boundary check
+            if (m == 0 or nums[m-1] != nums[m]) and (m == len(nums)-1 or nums[m] != nums[m+1]):
+                return nums[m]
             
-            # right side is even
-            if nums[mid] == nums[mid-1]:
-                if (r-mid) % 2 == 0:
-                    r = mid - 2
+            if nums[m] == nums[m-1]:
+                if (len(nums)-1-m) % 2 == 0:
+                    r = m - 2
                 else:
-                    l = mid + 1
-            elif nums[mid] == nums[mid+1]:
-                if (mid-l) % 2 == 0:
-                    l = mid + 2
+                    l = m + 1
+            else:
+                if (m) %2 == 0:
+                    l = m + 2
                 else:
-                    r = mid - 1
-                
-                
-                
-             
-
-
+                    r = m - 1
