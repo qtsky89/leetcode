d81ab44321b5725a912334c294db132edd4ef2df
Wonhee Jang <Qtsky89@gmail.com>
2024-12-31 01:07:43 +0900

    Time: 44 ms (44.77%) | Memory: 18.3 MB (7.99%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -8,20 +8,31 @@
 def is_identical(node1: TreeNode, node2: TreeNode) -> bool:
     if not node1 and not node2:
         return True
-    elif not (node1 and node2):
-        return False
-    elif node1.val != node2.val:
-        return False
     
-    return is_identical(node1.left, node2.left) and is_identical(node1.right, node2.right)
+    if not(node1 and node2):
+        return False
+        
+    return node1.val == node2.val and is_identical(node1.left, node2.left) and is_identical(node1.right, node2.right)
 
 
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:           
-        if not root:
-            return False
-
-        if (root and subRoot) and root.val == subRoot.val and is_identical(root, subRoot):
-            return True
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        ret = False
+        def dfs(node: TreeNode) -> None:
+            if not node:
+                return
             
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+            if node.val == subRoot.val and is_identical(node, subRoot):
+                nonlocal ret
+                ret = True
+                return
+            
+            dfs(node.left)
+            dfs(node.right)
+
+            
+        dfs(root)
+
+        
+        return ret
+        
