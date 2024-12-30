884d2208fc022ab6956b059cc94a34fefd13d880
Wonhee Jang <Qtsky89@gmail.com>
2024-12-30 18:33:37 +0900

    Time: 59 ms (19.71%) | Memory: 24.7 MB (8.09%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -1,54 +1,46 @@
-from collections import defaultdict, deque
-
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         '''
-        adjacent_map = {
-            1: [2, 3]
-            2: [1, 4]
-            3: [1]
-            4: [2]          
-        }
+        Bipartite graph
 
-        # blue => 1, red => -1
-        colors_map = {
-            1 : 0
-            2 : 0
-            3 : 0
-            4 : 0
-        }
 
-        if already has color and that color doesn't match:
-            return False
-        else
-            return True
+        colors = [0 0 0 0] BLUE: 1, RED: -1
+
+        1. change this colors array,when processing dislikes
+
+        2. it's already occupied, and has different color, return False
+
+        return True
         '''
 
         adjacent_map = defaultdict(list)
         for a, b in dislikes:
             adjacent_map[a].append(b)
             adjacent_map[b].append(a)
-        
-        colors_map = defaultdict(int)
+        colors = defaultdict(int)
 
         ret = True
-        # current_color => 1(BLUE), -1(RED) 0(WHITE)
+        # it will update the color
         def dfs(current_people: int, current_color: int) -> None:
             nonlocal ret
-
-            if not ret:
+            if ret is False:
                 return
 
-            colors_map[current_people] = current_color
+            colors[current_people] = current_color
 
-            for next_people in adjacent_map[current_people]:
-                if colors_map[next_people] == current_color:
+            for dislike_people in adjacent_map[current_people]:
+                # change ret
+                if colors[dislike_people] != 0 and colors[dislike_people] != (current_color * (-1)):
                     ret = False
-                if colors_map[next_people] == 0:
-                    dfs(next_people, current_color * (-1))
+                    return
                 
-        for current_people in range(1, n):
-            if colors_map[current_people] == 0:
-                dfs(current_people, 1)
+                if colors[dislike_people] == 0:
+                    dfs(dislike_people, current_color * (-1))
+        
+        for i in range(1, n+1):
+            if colors[i] == 0:
+                dfs(i, 1)
         
         return ret
+
+        
