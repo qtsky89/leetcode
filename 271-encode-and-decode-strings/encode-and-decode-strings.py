248b77bf6f2e54988753ca925f85d47c59633f2f
Wonhee Jang <Qtsky89@gmail.com>
2024-12-30 21:53:35 +0900

    Time: 78 ms (23.03%) | Memory: 18.1 MB (17.46%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -2,49 +2,45 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
+        '''
+        strs = hello world
+
+                5#hello5#hello
+                       
 
         '''
-        encoded_str = 5*Hello5*World
-        '''
-
-        ret: list[str] = []
+        ret = []
 
         for s in strs:
-            ret.append(f'{len(s)}*{s}')
-        
-        return ''.join(ret)
+            ret.append(f'{len(s)}#{s}')
 
+        return ''.join(ret)
+        
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
+        
+        5 #   h  e l l o 5 # h e l l o
+        i j j+1        ^       
         """
 
-        '''
-        encoded_str = '5 * H e l l o 5 * W o r l d'
-                      i  j     
-
-        => ['Hello', 'World']
-        
-        '''
-
-        if s == '':
-            return []
-
         ret = []
+
         i = 0
         while i <= len(s)-1:
-            j = i
-            while j <= len(s)-1 and s[j] != '*':
-                j += 1
-            length = int(s[i:j])
+            if s[i].isdigit():
+                j = i
 
-            ret.append(s[j+1:j+1+length])
-
-            i = (j+1+length)
+                while s[j] != '#':
+                    j += 1
+                
+                length = int(s[i:j])
+                
+                ret.append(s[j+1:j+1+length])
+                
+                i = j+1+length
         
         return ret
-                
-
 
 
         
