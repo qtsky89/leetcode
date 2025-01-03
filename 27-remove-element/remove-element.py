87edd4525dd0a88659f79d3576a95ec0e80f7601
Wonhee Jang <Qtsky89@gmail.com>
2025-01-04 00:22:53 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (13.56%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,44 +1,27 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         '''
-        example1.
-            nums = 3 2 2 3
-                   ^   ^
-                   2 2 3 3
+        nums = [4, 1, 2, 2, 3, 0, 0, 2] val = 2
+                l                 r
 
-
-            nums = 2 2 return 2
-
-
-        example2.
-            nums = 0 1 2 2 3 0 4 2
-                       ^       ^
-                   0 1 4 2 3 0 2 2
-                         ^   ^
-                   0 1 4 0 3 2 2 2
+             => [0, 1, 3, 0, 4        ] in place, and return 5
         '''
 
-        p1, p2 = 0, len(nums)-1
-
-        # p1 should be pointing val
-        # p2 should be not pointing val
-        # switch
-        # until p1 < p2
-
-        while p1 < p2:
-            while p1 < p2 and nums[p1] != val:
-                p1 += 1
-            
-            while p1 < p2 and nums[p2] == val:
-                p2 -= 1
-            
-            nums[p1], nums[p2] = nums[p2], nums[p1]
-        
         count = 0
-        for n in nums:
-            if n == val:
+        for i in range(len(nums)):
+            if nums[i] != val:
                 count += 1
-        
-        return len(nums) - count
 
+        l, r = 0, len(nums)-1
+        while l < r:
+            while l < r and nums[l] != val:
+                l += 1
+            
+            while l < r and nums[r] == val:
+                r -= 1
+            
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+        
+        return count
         
