9ceba25c9a09e51020c29cb24c0aad20a05d0f08
Wonhee Jang <Qtsky89@gmail.com>
2025-01-05 23:48:36 +0900

    Time: 274 ms (17.10%) | Memory: 22.3 MB (6.08%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,43 +1,52 @@
 class Solution:
+    def days(self, weights: list[int], capacity: int) -> int:
+        '''
+        weight = 1, 2, 3, 1, 1    4days
+
+                (1, 2) (3) (1, 1)
+        
+                 
+
+
+        capacity: 3 => 3days
+        
+        '''
+
+        count = 1
+        total = 0
+        for i in range(len(weights)):
+            if total + weights[i] > capacity:
+                count += 1
+                total = 0
+            total += weights[i]
+        
+        return count
+        
+
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         '''
-        capacity = start from max(weight)
-                   end sum(weight)
+        weight = 1, 2, 3, 4, 5, 6, 7, 8, 10
 
-        weight = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10     days = 5
-                 
-        capacity = 10 => (1, 2, 3, 4) (5) (6) (7) (8) (9) (10) => 7 days
-                   11 => (1, 2, 3, 4) (5, 6), (7) (8) (9) (10) => 6 days
-                   12 => (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
-                   13 => (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
-                   14 => (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
-                   15 => (1, 2, 3, 4) (5, 6) (7, 8) (9) (10) => 5 days
-                          1  3  6  10  5  6   7  8  9    10
-                          total = 9
-                          tmp_day = 4
+        find lowest capacity as possible
+
+        capacity => min(weight), 11, 12, 13 ...      sum(weight)
+                      ^                   m                ^
         '''
 
         l, r = max(weights), sum(weights)
 
         while l <= r:
-            m = l + (r-l) // 2
-
-            i = 0
-            total = 0
-            tmp_day = 1
-            while i <= len(weights)-1:
-                if total + weights[i] > m:
-                    tmp_day += 1
-                    total = 0
-                total += weights[i]
-
-                i += 1
+            m = l + (r-l)//2
             
-            if tmp_day <= days:
+            tmp_day = self.days(weights, m)
+
+            if tmp_day < days:
                 r = m - 1
-            else:
+            elif tmp_day > days:
                 l = m + 1
+            else:
+                r = m - 1
         
         return l
-         
-                
+
+        
