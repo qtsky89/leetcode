1097a739d98ed8cfa3b74f81c41d7090db6739b9
Wonhee Jang <Qtsky89@gmail.com>
2025-01-05 20:20:12 +0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (12.43%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,47 +1,40 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        '''
+        3 [ a 2 [ c ] ]
+                  ^
 
+        stack = 3 [ a 2
         '''
-            23[abc]
-            i
-              j
-        '''
-        i=0
-        while i <= len(s)-1:
+
+        stack = []
+        i = 0
+        while i<=len(s)-1:
             if s[i].isdigit():
                 j = i
-                while j <= len(s)-1 and s[j].isdigit():
-                    j += 1
-                stack.append(int(s[i:j]))
 
+                while s[j].isdigit():
+                    j += 1
+                
+                stack.append(int(s[i:j]))
                 i = j
-                continue
             elif s[i] == '[':
                 stack.append(s[i])
                 i += 1
             elif s[i].isalpha():
                 stack.append(s[i])
                 i += 1
-            elif s[i] == ']':
+            elif s[i] == ']': # pop the all value including open braket
                 tmp = ''
                 while stack and stack[-1] != '[':
                     tmp = stack.pop() + tmp
                 
-                '''
-                tmp = abc
-                '''
-
-                # remove open bracket
+                # remove open bracket. not using that
                 stack.pop()
 
-                # 23, 2, 3
-                times = stack.pop()
-                
-                stack.append(tmp * times)
+                # count
+                count = stack.pop()
+                stack.append(tmp * count)
                 i += 1
         
         return ''.join(stack)
-
-
-                
