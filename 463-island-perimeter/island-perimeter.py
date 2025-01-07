67564d110aaf0cad6cc8aadab73903cc139115dc
Wonhee Jang <Qtsky89@gmail.com>
2025-01-07 23:00:05 +0900

    Time: 57 ms (55.39%) | Memory: 18.2 MB (43.03%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,19 +1,21 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        count = 0        
+        total = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     c = 4
 
                     if i+1 <= len(grid)-1 and grid[i+1][j] == 1:
-                        c -=1
-                    if i-1 >= 0 and grid[i-1][j] == 1:
-                        c -=1
+                        c -= 1
                     if j+1 <= len(grid[0])-1 and grid[i][j+1] == 1:
+                        c -= 1
+                    if i-1 >= 0 and grid[i-1][j] == 1:
                         c -= 1
                     if j-1 >= 0 and grid[i][j-1] == 1:
                         c -= 1
-                    count += c
-        return count
-        
+                    
+                    total += c
+
+        return total
+                    
