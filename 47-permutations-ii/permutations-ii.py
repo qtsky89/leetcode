2041e4e0ccd27cb783dd8d3312d33e9ade2a7bb3
Wonhee Jang <Qtsky89@gmail.com>
2025-01-07 21:05:54 +0900

    Time: 32 ms (22.88%) | Memory: 17.8 MB (42.95%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        '''
+        nums = [1, 1, 2]
+
+        output = [1, 1, 2] [1, 2, 1] [2, 1, 1]
+        '''
+
+        nums.sort()
+
+        ret: list[list[int]] = []
+
+        def dfs(current_work: list[int], visited: set[int]) -> None:
+            # ending condition
+            if len(current_work) == len(nums):
+                ret.append(current_work)
+                return
+            
+            for i in range(len(nums)):
+                if i-1 in visited and (i-1 >= 0 and nums[i] == nums[i-1]):
+                    continue
+                if i not in visited:
+                    dfs(current_work + [nums[i]], visited | {i})
+        dfs([], set())
+
+        return ret
+
+        
