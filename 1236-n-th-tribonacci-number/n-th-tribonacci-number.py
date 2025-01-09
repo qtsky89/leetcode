0ffa1ad119eb54a8401aebf357b781e01b2ca207
Wonhee Jang <Qtsky89@gmail.com>
2025-01-09 18:52:51 +0900

    Time: 0 ms (100.00%) | Memory: 17.5 MB (36.17%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,18 +1,9 @@
 class Solution:
-    def __init__(self):
-        self._cache = {0: 0, 1: 1, 2: 1}
+    def __init__(self) -> None:
+        self.cache: dict[int, int] = {0: 0, 1: 1, 2: 1}
 
     def tribonacci(self, n: int) -> int:
-        '''
-        t(0) = 0
-        t(1) = 1
-        t(2) = 1
+        if n not in self.cache:
+            self.cache[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
 
-        t(n+3) = t(n) + t(n+1) + t(n+2)
-        '''
-
-        if n not in self._cache:
-            self._cache[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
-        return self._cache[n]
-
-        
+        return self.cache[n]
