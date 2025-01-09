4d4d4c7ca494dcb5849eea8ea6624d2982dcb004
Wonhee Jang <Qtsky89@gmail.com>
2025-01-09 16:50:34 +0900

    Time: 1 ms (13.49%) | Memory: 18 MB (10.66%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,58 +1,51 @@
+from collections import deque
+
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         '''
-        a / b = 2
-        b / c = 3
-
-        a / c => 6
-
-        {
-            a : [b, 2]
-            b : [a, 1/2]
+        adjacent_map = {
+            a : (b, 2.0)
+            ^    ^
+            b : (a, 0.5)
             ...
         }
 
+        [a, c]
+         ^  ^
+
+
         '''
 
-        hash_map = {}
+        adjacent_map = {}
 
-        for i in range(len(equations)):
-            x, y = equations[i]
-            value = values[i]
-
-            if x in hash_map:
-                hash_map[x].append([y, value])
+        for index, (start, end) in enumerate(equations):
+            if start not in adjacent_map:
+                adjacent_map[start] = [(end, values[index])]
             else:
-                hash_map[x] = [[y, value]]
-            
-            if y in hash_map:
-                hash_map[y].append([x, 1/value])
+                adjacent_map[start].append((end, values[index]))
+            if end not in adjacent_map:
+                adjacent_map[end] = [(start, 1/values[index])]
             else:
-                hash_map[y] = [[x, 1/value]]
+                adjacent_map[end].append((start, 1/values[index]))
 
-        q = deque()
-        for index, query in enumerate(queries):
-            q.append([index, query[0], query[1], 1, set()])
-
-        ret = [-1] * len(queries)
-
-        while q:
-            index, current_char, end_char, current_cal, visited = q.popleft()
-
-            if current_char == end_char and current_char in hash_map:
-                ret[index] = current_cal
-                continue
+        def dfs(current_char: str, end_char: str, current_cal: float, visited: set[str]) -> float:
+            if current_char not in adjacent_map:
+                return -1.0
             
-            if current_char not in visited:
-                visited.add(current_char)
+            if current_char == end_char:
+                return current_cal
             
-            if current_char in hash_map:
-                for next_char, cal in hash_map[current_char]:
-                    if next_char not in visited:
-                        q.append([index, next_char, end_char, current_cal * cal, set(visited)])
-        return ret
+            visited.add(current_char)
             
-            
+            for next_char, value in adjacent_map[current_char]:
+                if next_char not in visited:
+                    ret = dfs(next_char, end_char, current_cal * value, set(visited))
+                    if ret != -1.0:
+                        return ret
+            return -1.0
 
-
+        ret = []
+        for start, end in queries:
+            ret.append(dfs(start, end, 1, set()))
         
+        return ret
