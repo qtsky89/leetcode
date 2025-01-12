192b76d7ed2d31528c8b3ea2d77134963d097ed6
Wonhee Jang <Qtsky89@gmail.com>
2025-01-12 16:12:52 +0900

    Time: 53 ms (5.14%) | Memory: 20.3 MB (37.49%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,72 +1,51 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         '''
-        nums = [1, 1, 1],  k = 2
+        nums = [1, 1, 1] k = 2
                 ^  ^
-                   ^  ^ 
-                           ret=2
-
-        prefix_nums = [0, 1, 2, 3]
-                       ^     ^
-                           ^    ^
-
-
-        
-        nums = [1, 2, 3], k = 3
+                   ^  ^
+        nuns = [1, 2, 3] k = 3
                 ^  ^
-                      ^^
-                           ret=2
+                      ^
+
+        prefix_nums = [0, 1, 3, 6]
+                       
+        find i, j combination that 
         
-
-        prefix_nums = [0, 1, 3, 6] k = 3
-
-        i=0, j=2
-        i=2, j=3
-            
         prefix_nums[j] - prefix_nums[i] = k
-
-
-
-               nums = [1, -1, 0], k=0
-                       ^   ^
-                              ^^
-         prefix_nums= [0, 1, 0, 0] k = 0
-                       i     j
-
         '''
 
-        prefix_nums = [0] * (len(nums))
+        prefix_nums = [0] * len(nums)
 
         for i in range(0, len(nums)):
-            prefix_nums[i] = (prefix_nums[i-1] if i-1 >= 0 else 0) + nums[i]
+            prefix_nums[i] += (prefix_nums[i-1] if i-1 >= 0 else 0) + nums[i]
         
         prefix_nums.insert(0, 0)
-       
+
+        # find i, j combination that  i <= j prefix_nums[j] - prefix_nums[i] = k
+
+        #  prefix_nums = [0, 1, 3, 6] , k=3
+                                   
+          
+        '''
+            count = 1
+
+            hash_map = {
+                0 : 1
+                1 : 1
+                3 : 1
+                6 : 1
+            }
+        '''
+                    
         count = 0
-        '''
-
-        nums = [1, 2, 3], k = 3
-
-        prefix_nums = [0, 1, 3, 6] 
-                       ^     ^
-                             ^  ^
-        
-        prefix_nums[j] - prefix_nums[i] = k       => find all combination of (i, j)
-    
-        '''
         hash_map = {}
-        
         for i in range(len(prefix_nums)):
-            if (prefix_nums[i] - k) in hash_map:
+            if prefix_nums[i] - k in hash_map:
                 count += hash_map[prefix_nums[i] - k]
-            
-            if prefix_nums[i] in hash_map:
-                hash_map[prefix_nums[i]] += 1
-            else:
+
+            if prefix_nums[i] not in hash_map:
                 hash_map[prefix_nums[i]] = 1
-
+            else:
+                hash_map[prefix_nums[i]] += 1
         return count
-
-
-        
-        
