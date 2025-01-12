f5903eb3d3203c33698b65cbe7b1955d23ca120d
Wonhee Jang <Qtsky89@gmail.com>
2025-01-12 17:03:39 +0900

    Time: 23 ms (26.83%) | Memory: 19.1 MB (32.86%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,45 +1,44 @@
 from bisect import bisect_left
-from collections import deque
 
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        '''
-        arr = 1  2  3  4  5    k=4   x=3
-                 a  x     b
+        # find the insert index using binary search
+        index = bisect_left(arr, x)-1
 
-                i=2, j=2
-                i=1, j=3
-        
-        '''
+        if index < 0:
+            index = 0
 
-        if len(arr) == 1:
-            return arr
-
-        index = bisect_right(arr, x)-1
-        if index+1 <= len(arr)-1 and abs(x-arr[index]) > abs(x-arr[index+1]):
+        if index+1 <= len(arr)-1 and (abs(x-arr[index]) > abs(x-arr[index+1])):
             index += 1
 
+        # start, end
         q = deque([(index, index)])
-
+        
         while q:
             i, j = q.popleft()
-
+            
             if j-i+1 == k:
                 return arr[i:j+1]
             
-            if i-1 >= 0 and j+1 <= len(arr)-1:
-                if abs(x - arr[i-1]) <= abs(x - arr[j+1]):
+            # left expand
+            if (j+1 <= len(arr)-1 and i-1 >=0):
+                if abs(arr[i-1] - x) <= abs(arr[j+1]-x):
                     q.append((i-1, j))
                 else:
                     q.append((i, j+1))
-            elif i-1 >= 0:
+            elif i-1 >=0:
                 q.append((i-1, j))
             else:
                 q.append((i, j+1))
+
+            
+
         
+        
+
         
 
 
         
-        
+
         
