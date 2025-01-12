9b6439c71f85a98f63a1870aa9c10448cd8d1c48
Wonhee Jang <Qtsky89@gmail.com>
2025-01-12 17:24:04 +0900

    Time: 67 ms (17.32%) | Memory: 18.9 MB (20.91%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,63 +1,48 @@
 class Solution:
     def calculate(self, s: str) -> int:
         '''
-        s = " ( 123 + ( 4 + 5 + 2 ) - 3 ) + ( 6 + 8 ) "
-                                    ^
-
-
-        stack = 8, 14,
-            23
+        s = ( 1 + ( 4 + 5 + 2 ) - 3 ) + ( 6 + 8 )
+                              ^
+        stack = 8 14
         '''
 
-        stack = []
-
         i = 0
-        while i <= len(s)-1:
-            if s[i] == " " or s[i] == "+":
+        stack = []
+        while i < len(s):
+            if s[i] == '(':
+                stack.append(s[i])
                 i += 1
-                continue
-            
-            if s[i].isdigit():
-                j=i
-                while j <= len(s)-1 and s[j].isdigit():
+            elif s[i].isdigit():
+                j = i
+                while j < len(s) and s[j].isdigit():
                     j += 1
-                
+                number = int(s[i:j])
+
                 if stack and stack[-1] == '-':
                     stack.pop()
-                    stack.append(int(s[i:j]) * (-1)) # number    
-                else:
-                    stack.append(int(s[i:j])) # number
-                
-                i=j
+                    number = -number
+
+                stack.append(number)
+                i = j
+            elif s[i] == '+' or s[i] == ' ': # ignore
+                i += 1
             elif s[i] == '-':
                 stack.append(s[i])
                 i += 1
-            elif s[i] == '(':
-                stack.append(s[i])
-                i += 1
-            elif s[i] == ')':
-                ret = 0
-                while stack and stack[-1] != "(":
-                    ret += stack.pop()
-                stack.pop()
-
+            elif s[i] == ')': # pop out stacks
+                number = 0
+                
+                while stack and stack[-1] != '(':
+                    number += stack.pop()
+                
+                stack.pop() # remove open parenthesis (
+                
                 if stack and stack[-1] == '-':
                     stack.pop()
-                    stack.append(ret * (-1)) # number
-                else:
-                    stack.append(ret) # number
-                i+=1
+                    number = -number
+                stack.append(number)    
+                i += 1
+
+        print(stack)
 
         return sum(stack)
-                
-                
-                
-            
-
-
-
-                
-
-
-
-
