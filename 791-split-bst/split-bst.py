ce6d7781c9bdf261451bf18b5b38e0e363b5b958
Wonhee Jang <Qtsky89@gmail.com>
2025-01-14 00:05:25 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (24.52%) - LeetSync

--- 791-split-bst/split-bst.py
+++ 791-split-bst/split-bst.py
@@ -8,17 +8,16 @@ class Solution:
     def splitBST(self, root: Optional[TreeNode], target: int) -> List[Optional[TreeNode]]:
         if not root:
             return [None, None]
-        
+
         if root.val <= target:
             left, right = self.splitBST(root.right, target)
 
             root.right = left
 
-            return root, right
+            return [root, right]
         else:
             left, right = self.splitBST(root.left, target)
 
             root.left = right
 
-            return left, root
-            
+            return [left, root]
