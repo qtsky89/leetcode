5632ea889ce19534f03e95492308fb3dca305540
Wonhee Jang <Qtsky89@gmail.com>
2025-01-13 18:00:32 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (21.46%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,37 +1,37 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         '''
+        Input: s = " ( * ) ) "
+                             ^
 
-        s =  ( * ) )
-                   ^
+        Output: true
 
-        left = []
-        asterisk = [1]
-
+        left_stack = 
+        asterisk_stack =  
         '''
 
-        left: list[str] = []
-        asterisk: list[str] = []
+        left_stack = []
+        asterisk_stack = []
 
         for i in range(len(s)):
             if s[i] == '(':
-                left.append(i)
+                left_stack.append(i)
             elif s[i] == '*':
-                asterisk.append(i)
+                asterisk_stack.append(i)
             elif s[i] == ')':
-                if not left and not asterisk:
+                if (not left_stack) and (not asterisk_stack):
                     return False
-                
-                if left:
-                    left.pop()
-                elif asterisk:
-                    asterisk.pop()
+                elif left_stack:
+                    left_stack.pop()
+                elif asterisk_stack:
+                    asterisk_stack.pop()
 
-        while left and asterisk:
-            left_index = left.pop()
-            right_index = asterisk.pop()
+        while left_stack and asterisk_stack:
+            tmp_left = left_stack.pop()
+            tmp_asterisk = asterisk_stack.pop()
 
-            if left_index > right_index:
+            # * (
+            if tmp_asterisk < tmp_left:
                 return False
-        
-        return not left
+
+        return not left_stack
