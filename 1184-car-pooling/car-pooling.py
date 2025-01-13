a1d494d1a7065ad1eb4d2d65e3e79be412d879e4
Wonhee Jang <Qtsky89@gmail.com>
2025-01-13 12:18:14 +0900

    Time: 2 ms (72.60%) | Memory: 18.2 MB (29.54%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,43 +1,40 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         '''
-        example1.
-            trips = [[2,1,5], [3,3,7]], capacity = 4
-                        ^         
-            2
-            1  2  3  4  5  6  7
-            ^           ^
+        [2, 1, 5] [3, 3, 7]
+                      ^
 
-                  3=>2 1 passenger left in 3
-            1  2  3  4  5  6  7
-                  ^           ^
-
-            return False
-
-            1  2  3  4  5  6  7
-            ^
-        
+        heap = [(5, 2)]
+                 ^  ^
+        destination, passnger
         '''
 
+        # sort by from
+        trips.sort(key=lambda x:x[1])
 
-        # trips = [[2,1,5], [3,3,7]]
-        trips.sort(key = lambda x:x[1])
-
-        # heap
+        '''
+        (5, 2)
+         ^  ^
+        to_i, passenger
+        '''
         heap = []
 
-        for num_passenger, start, end in trips:
-            # TODO. add more capacity in current_capacity
-            while heap and heap[0][0] <= start:
-                _, num_drop_passenger = heapq.heappop(heap)
-                capacity += num_drop_passenger
+        for passenger, from_i, to_i in trips:
+            # heap process
+            # heap[0][0] => to_i
+            while heap and heap[0][0] <= from_i:
+                tmp_to, tmp_passenger =  heapq.heappop(heap)
+                capacity += tmp_passenger
 
-            if capacity < num_passenger:
+            # don't have enough capacity        
+            if capacity < passenger:
                 return False
+            
+            capacity -= passenger
 
-            # 4 - 2 => 2
-            capacity -= num_passenger
-
-            # add 5 to the heap
-            heapq.heappush(heap, (end, num_passenger))
+            heapq.heappush(heap, (to_i, passenger))
+        
         return True
+
+
+
