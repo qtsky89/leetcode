8b47b6a0019ae1a51dd4be88821594801d02a6ca
Wonhee Jang <Qtsky89@gmail.com>
2025-01-14 14:29:59 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (14.71%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,30 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        '''
-        s = c a t s a n d o g           wordDict = cats, dog, sand, and, cat 
-            ^
-        '''
-
         ret = False
+
         visited = set()
-
-        def dfs(current_index: index) -> None:
-            nonlocal ret
-            if ret:
-                return
-
-            if current_index == len(s):
+        def dfs(i: int) -> None:
+            if i == len(s):
+                nonlocal ret
                 ret = True
                 return
             
-            visited.add(current_index)
+            if ret:
+                return
+
+            visited.add(i)
             
             for word in wordDict:
-                if s[current_index:current_index+len(word)] == word and current_index+len(word) not in visited:
-                    dfs(current_index+len(word))
-        
+                if s[i:i+len(word)] == word and i+len(word) not in visited:
+                    dfs(i+len(word))
+            
         dfs(0)
 
         return ret
-
-        
