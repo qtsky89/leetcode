111ad8620652ce209f9d20c6a37812f7d2c5d267
Wonhee Jang <Qtsky89@gmail.com>
2025-01-14 16:42:18 +0900

    Time: 69 ms (16.24%) | Memory: 18.9 MB (24.04%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,46 +1,48 @@
 class Solution:
     def calculate(self, s: str) -> int:
         '''
-        s = ( 1 + ( 4 + 5 + 2 ) - 3 ) + ( 6 + 8 )
-                              ^
-        stack = 8 14
+        s = " ( 1 + ( 4 + 5 + 2 ) - 3 ) + ( 6 + 8 )"
+
+        stack = [8, 14]
+
+        return sum(stack)
+
         '''
 
         i = 0
-        stack = []
-        while i < len(s):
-            if s[i] == '(':
+        stack: list[int|str] = []
+
+        while i <= len(s)-1:
+            if s[i] in '+ ':
+                i += 1
+            elif s[i] in '(-':
                 stack.append(s[i])
                 i += 1
             elif s[i].isdigit():
                 j = i
-                while j < len(s) and s[j].isdigit():
+                while j <= len(s)-1 and s[j].isdigit():
                     j += 1
                 number = int(s[i:j])
 
                 if stack and stack[-1] == '-':
                     stack.pop()
                     number = -number
-
+                
                 stack.append(number)
                 i = j
-            elif s[i] == '+' or s[i] == ' ': # ignore
-                i += 1
-            elif s[i] == '-':
-                stack.append(s[i])
-                i += 1
-            elif s[i] == ')': # pop out stacks
-                number = 0
-                
+            elif s[i] == ')':
+                number = 0 
+
                 while stack and stack[-1] != '(':
                     number += stack.pop()
                 
-                stack.pop() # remove open parenthesis (
-                
+                # pop out (
+                stack.pop()
+
                 if stack and stack[-1] == '-':
                     stack.pop()
                     number = -number
-                stack.append(number)    
+                
+                stack.append(number)
                 i += 1
-
         return sum(stack)
