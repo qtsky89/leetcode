ce200876988c57fe886ec1e75b75b284fef286e6
Wonhee Jang <Qtsky89@gmail.com>
2025-01-14 17:05:06 +0900

    Time: 137 ms (83.80%) | Memory: 24.1 MB (14.02%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -4,19 +4,17 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         q = deque([(0, len(matrix[0])-1)])
 
-        # time: O(M+N) space: O(M+N)
         while q:
             i, j = q.popleft()
 
-            if not (0 <= i <= len(matrix)-1 and 0 <= j <= len(matrix[0])-1):
+            if not (0 <= i <= len(matrix)-1 ) or not ( 0 <= j <= len(matrix[0])-1 ):
                 continue
 
             if matrix[i][j] == target:
                 return True
             elif matrix[i][j] > target:
                 q.append((i, j-1))
-            else:
+            elif matrix[i][j] < target:
                 q.append((i+1, j))
-
+        
         return False
-            
