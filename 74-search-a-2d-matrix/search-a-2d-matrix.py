2d692347728545bb8bface8b10b278960de5094e
Wonhee Jang <Qtsky89@gmail.com>
2025-01-14 14:10:38 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (44.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,40 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        '''
-         
-          1 3 5 7 
-        ->10 11 16 20   target = 16
-        23 30 34 60
-        
-        target
-
-        first  search through first column => [1, 10, 23]
-            find equal or less than target
-
-
-        second search through that row [1, 3, 5, 7]
-        '''
-
         t, b = 0, len(matrix)-1
 
         while t <= b:
-            mid = (t+b) // 2
+            m = t + (b-t) // 2
 
-            if matrix[mid][0] <= target:
-                t = mid + 1
+            if matrix[m][0] == target:
+                return True
+            elif matrix[m][0] < target:
+                t = m + 1
             else:
-                b = mid - 1
+                b = m - 1
+        
+        # t <= insert
 
-        row = (t+b) // 2
+        t = t - 1 if t - 1 >= 0 else t
 
         l, r = 0, len(matrix[0])-1
-        while l <= r:
-            mid = (l+r) // 2
 
-            if matrix[row][mid] == target:
+        while l <= r:
+            m = l + (r-l) // 2
+
+            if matrix[t][m] == target:
                 return True
-            elif matrix[row][mid] < target:
-                l = mid + 1
+            elif matrix[t][m] < target:
+                l = m + 1
             else:
-                r = mid - 1
+                r = m - 1
+            
         return False
