0f7f161f4bcc5d8367421640f8125fdcbaf885c5
Wonhee Jang <Qtsky89@gmail.com>
2025-01-15 00:07:29 +0900

    Time: 4 ms (63.01%) | Memory: 17.7 MB (42.17%) - LeetSync

--- 2881-split-strings-by-separator/split-strings-by-separator.py
+++ 2881-split-strings-by-separator/split-strings-by-separator.py
@@ -1,20 +1,11 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
-        '''
-        Input: words = ["one.two.three","four.five","six"], separator = "."
-
-        Output: ["one","two","three","four","five","six"]
-        '''
-
         ret = []
 
         for word in words:
             tmp = word.split(separator)
 
-            tmp_filter = []
             for t in tmp:
-                if t != "":
-                    tmp_filter.append(t)
-
-            ret += tmp_filter
+                if t != '':
+                    ret.append(t)
         return ret
