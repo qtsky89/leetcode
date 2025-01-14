3f22cb674826bcedec4b2642a6226aa127ced3f7
Wonhee Jang <Qtsky89@gmail.com>
2025-01-14 23:20:03 +0900

    Time: 39 ms (43.52%) | Memory: 19.3 MB (40.25%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,60 +2,55 @@ from collections import deque
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific_set = set()
-        atlantic_set = set()
+        pacific_ocean = set()
 
         q = deque()
-        # pacific
+
         for i in range(len(heights)):
             q.append((i, 0))
-        
         for j in range(len(heights[0])):
             q.append((0, j))
         
         while q:
             i, j = q.popleft()
-            
-            pacific_set.add((i, j))
+            pacific_ocean.add((i, j))
 
-            if i+1 <= len(heights)-1 and heights[i][j] <= heights[i+1][j] and (i+1, j) not in pacific_set:
+            if i+1 <= len(heights)-1 and heights[i][j] <= heights[i+1][j] and (i+1, j) not in pacific_ocean:
                 q.append((i+1, j))
-            if j+1 <= len(heights[0])-1 and heights[i][j] <= heights[i][j+1] and (i, j+1) not in pacific_set:
+            if j+1 <= len(heights[0])-1 and heights[i][j] <= heights[i][j+1]  and (i, j+1) not in pacific_ocean:
                 q.append((i, j+1))
-            if i-1 >= 0 and heights[i][j] <= heights[i-1][j] and (i-1, j) not in pacific_set:
+            if i-1 >= 0 and heights[i][j] <= heights[i-1][j]  and (i-1, j) not in pacific_ocean:
                 q.append((i-1, j))
-            if j-1 >= 0 and heights[i][j] <= heights[i][j-1] and (i, j-1) not in pacific_set:
+            if j-1 >= 0 and heights[i][j] <= heights[i][j-1]  and (i, j-1) not in pacific_ocean:
                 q.append((i, j-1))
         
-        
+        atlantic_ocean = set()
+
         q = deque()
-        # atlantic
         for i in range(len(heights)):
             q.append((i, len(heights[0])-1))
         
         for j in range(len(heights[0])):
-            q.append((len(heights)-1, j))
+            q.append((len(heights)-1 ,j))
+            
         
         while q:
             i, j = q.popleft()
-            
-            atlantic_set.add((i, j))
+            atlantic_ocean.add((i, j))
 
-            if i+1 <= len(heights)-1 and heights[i][j] <= heights[i+1][j] and (i+1, j) not in atlantic_set:
+            if i+1 <= len(heights)-1 and heights[i][j] <= heights[i+1][j]  and (i+1, j) not in atlantic_ocean:
                 q.append((i+1, j))
-            if j+1 <= len(heights[0])-1 and heights[i][j] <= heights[i][j+1] and (i, j+1) not in atlantic_set:
+            if j+1 <= len(heights[0])-1 and heights[i][j] <= heights[i][j+1] and (i, j+1) not in atlantic_ocean:
                 q.append((i, j+1))
-            if i-1 >= 0 and heights[i][j] <= heights[i-1][j] and (i-1, j) not in atlantic_set:
+            if i-1 >= 0 and heights[i][j] <= heights[i-1][j] and (i-1, j) not in atlantic_ocean:
                 q.append((i-1, j))
-            if j-1 >= 0 and heights[i][j] <= heights[i][j-1] and (i, j-1) not in atlantic_set:
+            if j-1 >= 0 and heights[i][j] <= heights[i][j-1] and (i, j-1) not in atlantic_ocean:
                 q.append((i, j-1))
-
-        ret_set = pacific_set & atlantic_set
+        
+        tmp = pacific_ocean & atlantic_ocean
 
         ret = []
-        for t in ret_set:
+        for t in tmp:
             ret.append(list(t))
+        
         return ret
-
-        
-        
