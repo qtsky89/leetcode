3181be8cb890a6c68f75743c6cc144959c4a5c18
Wonhee Jang <Qtsky89@gmail.com>
2025-01-15 18:56:52 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (16.00%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,42 +1,28 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         '''
-        [1, 2, 3, 4, 5]
-        [5, 1, 2, 3, 4] rotate 1
-        [4, 5, 1, 2, 3] rotate 2
-        [3, 4, 5, 1, 2] rotate 3
-        [2, 3, 4, 5, 1] rotate 4
-        [1, 2, 3, 4, 5] rotate 5
+        binary search can only work in sorted array
 
+        nums = [4, 5, 6, 7, 0, 1, 2]
+                            l  m   r
 
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        [10, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        [9, 10, 1, 2, 3, 4, 5, 6, 7, 8]
-
-                   *
-        [8, 9, 10, 1, 2, 3, 4, 5, 6, 7] n=3
                 
-
-        find this index (smallest nums[i] index)
-        nums[i-1] > nums[i]
         '''
-
         if len(nums) == 1:
             return nums[0]
 
+
         l, r = 0, len(nums)-1
 
-        while l<=r:
-            mid = l + (r-l) // 2
+        while l <= r:
+            m = l + (r-l) // 2
 
-            # find it!
-            if nums[mid-1] > nums[mid]:
-                return nums[mid]
-            elif nums[mid] < nums[r]:
-                r = mid - 1
+            # smallest target
+            if nums[m-1] > nums[m]:
+                return nums[m]
+            elif nums[m] < nums[r]:
+                r = m - 1
             else:
-                l = mid + 1
-    
-    '''
-        time complexity: O(logN) space compexity: O(1)
-    '''
+                l = m + 1
+        
+                
