76c185707b71540b77cec891348c32c6f37bcd42
Wonhee Jang <Qtsky89@gmail.com>
2025-01-15 17:38:01 +0900

    Time: 59 ms (31.15%) | Memory: 18.2 MB (9.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,36 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        i, j = 0, len(s1)-1
+        '''
+        s1 = abc
+        {
+            a : 1
+            b : 1
+            c : 1
+        }
 
-        s1_counter = {}
-        for s in s1:
-            if s in s1_counter:
-                s1_counter[s] += 1
-            else:
-                s1_counter[s] = 1
+        s2 = e i d b a c o o o
+             l   r
+        {
+            e : 1
+            i : 1
+            d : 1
+        }
+        '''
 
-        s2_counter = {}
-        for s in s2[:len(s1)]:
-            if s in s2_counter:
-                s2_counter[s] += 1
-            else:
-                s2_counter[s] = 1
-
-        while j <= len(s2)-1:
-            if s1_counter == s2_counter:
-                return True
-            
-            if j+1 <= len(s2)-1:
-                if s2[j+1] in s2_counter:
-                    s2_counter[s2[j+1]] += 1
-                else:
-                    s2_counter[s2[j+1]] = 1
-            
-                s2_counter[s2[i]] -= 1
-                if s2_counter[s2[i]] == 0:
-                    del s2_counter[s2[i]]
-
-            i, j = i+1, j+1
+        c1 = Counter(s1)
         
+        c2 = Counter(s2[:len(s1)])
+
+        l, r = 0, len(s1)-1
+
+        while r <= len(s2)-1:
+            if c1 == c2:
+                return True
+
+            c2[s2[l]] -= 1
+            if c2[s2[l]] == 0:
+                del c2[s2[l]]
+            l, r = l+1, r+1
+
+            if r <= len(s2)-1:
+                if s2[r] in c2:
+                    c2[s2[r]] += 1
+                else:
+                    c2[s2[r]] = 1
 
         return False
+        
