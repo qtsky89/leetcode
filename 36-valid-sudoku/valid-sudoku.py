927cdff7fa06be2ed94f93b33d8ecc09162ee0b4
Wonhee Jang <Qtsky89@gmail.com>
2025-01-15 17:14:17 +0900

    Time: 4 ms (60.95%) | Memory: 18 MB (13.02%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,40 +1,27 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # check every row
-        for i in range(len(board)):
+        # check all rows
+        for i in range(9):
             visited = set()
-
-            for j in range(len(board[0])):
-                if board[i][j] == '.':
-                    continue
-                
-                if board[i][j] in visited:
-                    return False
-                visited.add(board[i][j])
-
-        # check every column
-        for j in range(len(board[0])):
-            visited = set()
-
-            for i in range(len(board)):
-                if board[i][j] == '.':
-                    continue
-                
-                if board[i][j] in visited:
+            for j in range(9):
+                if board[i][j] != '.' and board[i][j] in visited:
                     return False
                 visited.add(board[i][j])
         
-        # check sub board
+        # check all columns
+        for j in range(9):
+            visited = set()
+            for i in range(9):
+                if board[i][j] != '.' and board[i][j] in visited:
+                    return False
+                visited.add(board[i][j])
+        
         for i in range(3):
             for j in range(3):
                 visited = set()
-
                 for k in range(3):
                     for l in range(3):
-                        if board[3*i+k][3*j+l] == '.':
-                            continue
-                        
-                        if board[3*i+k][3*j+l] in visited:
+                        if board[3*i+k][3*j+l] != '.' and board[3*i+k][3*j+l] in visited:
                             return False
                         visited.add(board[3*i+k][3*j+l])
         return True
