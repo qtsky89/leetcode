e1dce8d59ba041eaabacdef2593a28ce1249dc10
Wonhee Jang <Qtsky89@gmail.com>
2025-01-16 18:03:48 +0900

    Time: 23 ms (52.94%) | Memory: 18 MB (8.08%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,27 +1,25 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ret: list[list[int]] = []
+
         candidates.sort()
-        q = deque([[-1, []]])
+        # cadndiates = 1 1 2 5 6 7 10
+        #              ^ 
 
-        visited: set[tuple[int]] = set()
-        ret = []
-        while q:
-            last_index, current_work = q.popleft()
-
-            tmp_tuple = tuple(current_work)
-            if tmp_tuple in visited:
-                continue
-            else:
-                visited.add(tmp_tuple)
-
-            tmp = sum(current_work)
-            if tmp == target:
+        def dfs(current_index: int, current_sum: int, current_work: list[int]) -> None:
+            if current_sum == target:
                 ret.append(current_work)
-            elif tmp > target:
-                continue
+                return
+            elif current_sum > target:
+                return
+            elif current_index == len(candidates):
+                return
             
+            for i in range(current_index, len(candidates)):
+                if i > current_index and candidates[i] == candidates[i - 1]:
+                    continue
+                dfs(i+1, current_sum + candidates[i], current_work + [candidates[i]])
+            
+        dfs(0, 0, [])
 
-            for i in range(last_index+1, len(candidates)):
-                q.append([i, [*current_work, candidates[i]]])
-        
         return ret
