752340a81646a6ac3ede09394d3227d578c07ff8
Wonhee Jang <Qtsky89@gmail.com>
2025-01-17 19:50:17 +0900

    Time: 0 ms (100.00%) | Memory: 19.6 MB (36.45%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,24 +1,38 @@
-from bisect import bisect_left
-
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        index = bisect_left(intervals, newInterval)
-        intervals.insert(index, newInterval)
+        '''
+        [[1,2],[3,5],[6,7],[8,10],[12,16]]   [4, 8]
+          l                         r
 
-        filtered_intervals: list[list[int]] = []
+        '''
+
+        l, r = 0, len(intervals)-1
+
+        while l<=r:
+            m = l + (r-l) // 2
+
+            if intervals[m][0] == newInterval[0]:
+                r = m - 1
+            elif intervals[m][0] > newInterval[0]:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        # l is the inserting point
+
+        intervals.insert(l, newInterval)
+
+        ret = []
 
         for start, end in intervals:
-            if not filtered_intervals:
-                filtered_intervals.append([start, end])
-                continue
-            
-            # is merged ?
-            if filtered_intervals[-1][1] >= start:
-                filtered_intervals[-1][0] = min(filtered_intervals[-1][0], start)
-                filtered_intervals[-1][1] = max(filtered_intervals[-1][1], end)
+            # it need to be merged
+            if ret and ret[-1][1] >= start:
+                ret[-1][0] = min(ret[-1][0], start)
+                ret[-1][1] = max(ret[-1][1], end)
             else:
-                filtered_intervals.append([start, end])
+                ret.append([start, end])
         
-        return filtered_intervals
-            
-            
+        return ret
+
+        
+        
