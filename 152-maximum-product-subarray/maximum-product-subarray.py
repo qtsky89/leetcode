ddacd5b6fbda1bb40beed28da8f5edc2af5b79f6
Wonhee Jang <Qtsky89@gmail.com>
2025-01-17 12:12:35 +0900

    Time: 10 ms (59.23%) | Memory: 18.4 MB (32.49%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,29 +1,20 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        
         '''
+        nums = 2 3 -2   4  5  0  -1  -2  4
+               
+               2 6  6   4  20 0   0   2     max
+                  -12 -48 240 0  -1  -2     min
 
-        -4, -12, 24
-
-        nums =   -2   3   -4
-        c_max 1  -2   3    24
-        c_min 1  -2   -6   -12
-
-        c_max = max(nums[i], c_max * nums[i], c_min * nums[i])
-        c_min = min(nums[i], c_max * nums[i], c_min * nums[i])
         '''
-
-        c_max, c_min = 1, 1
-
         ret = max(nums)
+
+        max_so_far = 1
+        min_so_far = 1
+
         for n in nums:
-            c_max, c_min = max(n, c_max * n, c_min * n), min(n, c_max * n, c_min * n)
+            max_so_far, min_so_far = max(n, max_so_far * n, min_so_far * n), min(n, max_so_far * n, min_so_far * n)
 
-            ret = max(ret, c_max)
-        
+            ret = max(ret, max_so_far)
+
         return ret
-            
-
-
-
-        
