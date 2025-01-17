135f7341cc920432fb1f0aebada9fd6baccc8ab0
Wonhee Jang <Qtsky89@gmail.com>
2025-01-17 17:25:40 +0900

    Time: 167 ms (18.27%) | Memory: 19.3 MB (21.13%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,46 +1,76 @@
+from collections import Counter
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         '''
+        tasks = A A A B B B , n=2
+        
+        # step 1 counter (hashmap)
         c = {
             A : 3
             B : 3
         }
 
-        heap = [(-3, A), (-3, B)]
+        # step 2 min heap
+        heap = [(-3, 'A'), (-3, 'B')]
 
-        A B idle A B idle A B
 
-        
+        # step 3, while loop processing heap, cooldown_q
+        while 
+
+        cooldown_q = [(n+1 , (-2, A'))]
+
+
         '''
 
-        c = Counter(tasks)
+
+        '''
+        tasks = A A A B B B , n=2
         
+        # step 1 counter (hashmap)
+        c = {
+            A : 3
+            B : 3
+        }
+        '''
+        c = Counter(tasks)
+
+        '''
+        # step 2 min heap
+        heap = [(-3, 'A'), (-3, 'B')]
+        '''
         heap = []
 
-        for key, value in c.items():
-            heapq.heappush(heap, (-value, key))
+        for key in c:
+            heap.append((-c[key], key))
         
-        cooldown_q = deque()
-        # [time, item]
+        heapq.heapify(heap)
 
+        # step 3, while loop processing heap, cooldown_q
+
+        cooldown_q = deque()
         time = 0
         while heap or cooldown_q:
             time += 1
+            # TODO. push back to heap from cooldown_q
+            while cooldown_q and cooldown_q[0][0] <= time:
+                _, data = cooldown_q.popleft()
+                heapq.heappush(heap, data)
 
-            if cooldown_q and cooldown_q[0][0] <= time:
-                _, item = cooldown_q.popleft()
-                heapq.heappush(heap, item)
-            
+            # idle
             if not heap:
                 continue
-
+            
             count, char = heapq.heappop(heap)
+
             count += 1
 
-            if count == 0:
-                continue
-            else:
-                cooldown_q.append([time+n+1, (count, char)])
-            
+            if count != 0:
+                cooldown_q.append((time+n+1, (count, char)))
+        
+
         return time
+                
+
+
 
