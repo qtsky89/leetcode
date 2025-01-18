fa396f1660043ef0e35bd36b6d8eea8c3def27f0
Wonhee Jang <Qtsky89@gmail.com>
2025-01-18 21:53:13 +0900

    Time: 240 ms (25.19%) | Memory: 22 MB (47.17%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,27 +1,21 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         '''
-        weights = [1, 2, 3, 4, 5] days = 2
-                               ^
-        
-        capacity: 5 (1, 2) (3) (4) (5) => 5 days
-                  6 (1, 2, 3) (4) (5) => 3 days
-                  7 (1, 2, 3) (4) (5) => 3 days
-                  8 (1, 2, 3) (4) (5) => 3 days
-                  9 (1, 2, 3) (4, 5) =>  2 days
-                  10 (1, 2, 3, 4) (5) => 2 days
-                  11 (1, 2, 3, 4) (5) => 2 days
-                  12 (1, 2, 3, 4) (5) => 2 days
-                  13 (1, 2, 3, 4) (5) => 2 days
-                  14 (1, 2, 3, 4) (5) => 2 days
-                  15 (1, 2, 3, 4, 5) => 1 days
-        
-        capacity: 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-                  l                                   r
+        weights = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10      days= 5
+                                              ^
 
-        days    : 5, 3, 3, 3, 2,  2,  2,  2,  2,   2, 1
-                  l               m                    r
-                              
+        capacity 10: (1, 2, 3, 4) (5) (6) (7) (8) (9) (10) => 7 days
+        capacity 11: (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
+        capacity 12: (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
+        capacity 13: (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
+        capacity 14: (1, 2, 3, 4) (5, 6) (7) (8) (9) (10) => 6 days
+        capacity 15: (1, 2, 3, 4, 5) (6, 7) (8) (9) (10) => 5 days
+        
+
+        capacity: 10 11 12 13 14 15
+
+        days       7 6  6  6  6   5
+                                  ^
         '''
 
         l, r = max(weights), sum(weights)
@@ -29,19 +23,21 @@ class Solution:
         while l <= r:
             m = l + (r-l) // 2
 
-            # calculate days
+            # using this m capacity , let's convert to days
             current_sum = 0
-            day = 1
+            current_day = 1
             for weight in weights:
-                if current_sum + weight <= m:
-                    current_sum += weight
-                else:
-                    day += 1
+                if current_sum + weight > m:
                     current_sum = weight
-            ###
-
-            if day <= days:
+                    current_day += 1
+                else:
+                    current_sum += weight
+            
+            if current_day == days:
                 r = m - 1
-            else:
+            elif current_day > days:
                 l = m + 1
+            elif current_day < days:
+                r = m - 1
+
         return l
