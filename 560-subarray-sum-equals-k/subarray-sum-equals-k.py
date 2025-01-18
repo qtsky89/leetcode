b68c1dc50df4c7be032921c25fb2782269d6b993
Wonhee Jang <Qtsky89@gmail.com>
2025-01-18 15:48:50 +0900

    Time: 38 ms (36.59%) | Memory: 20.4 MB (25.93%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,51 +1,46 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         '''
-        nums = [1, 1, 1] k = 2
-                ^  ^
-                   ^  ^
-        nuns = [1, 2, 3] k = 3
-                ^  ^
-                      ^
+        nums = 1, 2, 3, 4, 5
+        prefix_sum = 0, 1, 3, 6, 10, 15                     k=3 
+                           i
 
-        prefix_nums = [0, 1, 3, 6]
-                       
-        find i, j combination that 
+                   we need to find i, j that prefix_sum[j] - prefix_sum[i] => k
+
+                   how many count (i, j) have ?
+        '''
+
+        prefix_sum = [0] * len(nums)
+        prefix_sum[0] = nums[0]
+
+        for i in range(1, len(prefix_sum)):
+            prefix_sum[i] = prefix_sum[i-1] + nums[i]
         
-        prefix_nums[j] - prefix_nums[i] = k
-        '''
+        prefix_sum.insert(0, 0)
 
-        prefix_nums = [0] * len(nums)
-
-        for i in range(0, len(nums)):
-            prefix_nums[i] += (prefix_nums[i-1] if i-1 >= 0 else 0) + nums[i]
-        
-        prefix_nums.insert(0, 0)
-
-        # find i, j combination that  i <= j prefix_nums[j] - prefix_nums[i] = k
-
-        #  prefix_nums = [0, 1, 3, 6] , k=3
-                                   
-          
-        '''
-            count = 1
-
-            hash_map = {
-                0 : 1
-                1 : 1
-                3 : 1
-                6 : 1
-            }
-        '''
-                    
         count = 0
-        hash_map = {}
-        for i in range(len(prefix_nums)):
-            if prefix_nums[i] - k in hash_map:
-                count += hash_map[prefix_nums[i] - k]
+        hash_map: dict[int, int] = {}
 
-            if prefix_nums[i] not in hash_map:
-                hash_map[prefix_nums[i]] = 1
+        for i in range(len(prefix_sum)):
+            if prefix_sum[i]-k in hash_map:
+                count += hash_map[prefix_sum[i]-k]
+            
+            if prefix_sum[i] in hash_map:
+                hash_map[prefix_sum[i]] += 1
             else:
-                hash_map[prefix_nums[i]] += 1
+                hash_map[prefix_sum[i]] = 1
         return count
+
+
+        # we need to find i, j that i < j and prefix_sum[j] - prefix_sum[i] => k
+        # for i in range(0, len(prefix_sum)-1):
+        #     for j in range(i+1, len(prefix_sum)):
+        #         if prefix_sum[j] - prefix_sum[i] == k:
+        #             count += 1
+
+        
+
+        
+
+
+
