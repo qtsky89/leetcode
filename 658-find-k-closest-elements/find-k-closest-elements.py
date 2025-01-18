0fed5fc42d77eb7372040c4233333055b0b06d78
Wonhee Jang <Qtsky89@gmail.com>
2025-01-18 17:31:45 +0900

    Time: 20 ms (27.89%) | Memory: 19.3 MB (20.54%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,44 +1,54 @@
-from bisect import bisect_left
+from collections import deque
 
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # find the insert index using binary search
-        index = bisect_left(arr, x)-1
+        '''
+        arr = 2, 4, 6, 9, 10          x = 8, k = 4 
+                       ^
 
-        if index < 0:
-            index = 0
 
-        if index+1 <= len(arr)-1 and (abs(x-arr[index]) > abs(x-arr[index+1])):
-            index += 1
+        '''
 
-        # start, end
-        q = deque([(index, index)])
+        # find right index to start
+
+        l, r = 0, len(arr)-1
+
+        while l <= r:
+            m = l + (r-l) // 2
+
+            if arr[m] == x:
+                r = m -1
+            elif arr[m] > x:
+                r = m - 1
+            else:
+                l = m + 1
+            
+        # l will pointing the insert index
+        # compare l, l-1 find root
+        
+        if l > len(arr)-1:
+            l -= 1
+        elif l-1 >= 0 and abs(arr[l-1] - x) <= abs(arr[l] - x):
+            l -= 1
+        
+            
+        # q = [(left_index, right_index)]
+        q = deque([(l, l)])
         
         while q:
             i, j = q.popleft()
-            
-            if j-i+1 == k:
+
+            if j - i + 1 == k:
                 return arr[i:j+1]
             
-            # left expand
-            if (j+1 <= len(arr)-1 and i-1 >=0):
-                if abs(arr[i-1] - x) <= abs(arr[j+1]-x):
+            if i-1 >= 0 and j+1 <= len(arr)-1:
+                if abs(x - arr[i-1]) <= abs(x - arr[j+1]):
                     q.append((i-1, j))
                 else:
                     q.append((i, j+1))
-            elif i-1 >=0:
+            elif i-1 >= 0:
                 q.append((i-1, j))
-            else:
+            elif j + 1 <= len(arr)-1:
                 q.append((i, j+1))
-
+        
             
-
-        
-        
-
-        
-
-
-        
-
-        
