1e4d5fa1e511f82a40cc606bfe96573debce7fc9
Wonhee Jang <Qtsky89@gmail.com>
2025-01-19 18:16:46 +0900

    Time: 3 ms (51.58%) | Memory: 17.8 MB (25.45%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,68 +1,72 @@
+from collections import deque
+
 class Solution:
     def reorganizeString(self, s: str) -> str:
         '''
-        s =  a a a a b b b c c d
+        s = a a a a b b b c c d
+               ^
+        c = {
+            a : 4
+            b : 3
+            c : 2
+            d : 1
+        }
 
-             a b a b a c a b c d
+        heap = (-4, a), (-3, b) (-2, c) (-1, d)
+                  ^
+        q = 
+
+        a b a c a b d a b c
+        '''
+
+        '''
+        c = {
+            a : 4
+            b : 3
+            c : 2
+            d : 1
+        }
+        '''
+        c = Counter(s)
         
-        count_map = {
-            a: 4
-            b: 3
-            c: 2
-            d: 1  
-        }
-
-        heap = [(-4, a), (-3, b), (-2, c), (-1, d)]
-
-        1. use highest count as possible
-        2. don't use same character again
+        '''
+        heap = (-4, a), (-3, b) (-2, c) (-1, d)
         '''
 
-        '''
-        count_map = {
-            a: 4
-            b: 3
-            c: 2
-            d: 1  
-        }
-        '''
-        count_map = Counter(s)
-
-        # heap = [(-4, a), (-3, b), (-2, c), (-1, d)]
         heap = []
-        for key, value in count_map.items():
-            heap.append((-value, key))
-        heapq.heapify(heap)
+        for key, value in c.items():
+            heapq.heappush(heap, (-value, key))
         
-        # to cooldown previous used character
-        # cooldown_q = [(time, (-4, a)), ...]
         cooldown_q = deque()
         ret = []
 
         time = 0
-
+        # processing the heap
         while heap or cooldown_q:
             time += 1
+
+            # pop item from cooldown_q and push the heap
             if cooldown_q and cooldown_q[0][0] <= time:
-                _, data = cooldown_q.popleft()
-                
-                heapq.heappush(heap, data)
+                _, (tmp_counter, tmp_char) = cooldown_q.popleft()
+
+                heapq.heappush(heap, (tmp_counter, tmp_char))
             
             if not heap:
                 return ""
             
+            # -4, a
             count, char = heapq.heappop(heap)
             ret.append(char)
+
             count += 1
 
-            if count < 0:
+            # push to the cooldown_q
+            if count != 0:
                 cooldown_q.append((time+2, (count, char)))
             
-        return ''.join(ret)
+
+        return ''.join(ret) if len(ret) == len(s) else ""
+
             
-            
-
-
-
-                
+        
         
