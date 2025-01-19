25d330e09d9c5912714d7c1a03f080d36c7974f4
Wonhee Jang <Qtsky89@gmail.com>
2025-01-19 17:27:54 +0900

    Time: 360 ms (51.37%) | Memory: 61.9 MB (5.73%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -1,50 +1,65 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         '''
-        tasks => [1, 2, 0] [2, 4, 1] [3, 2, 2] [4, 1, 3]
+        [1, 2]
+        [2, 4]
+        [3, 2]
+        [4, 1]
 
-        tasks.sort() => [1, 2, 0] [2, 4, 1] [3, 2, 2] [4, 1, 3]
-        
+
         time = 1
+            [1, 2]
+        
+        time = 3
+            [2, 4] [3, 2]
+                       ^
+        time = 5
+            [2, 4] [4, 1]
+                      ^
+        time = 9
+            [2, 4]
+              ^
+        time = 11
+            no job left
 
-        heap = [(2, 0),]
-                  ^
-        process_time, index = heappop(heap)
+        => [0, 2, 3, 1]
 
-        ret.append(index)
+        tasks = [[1, 2, 0], [2, 4, 1], [3, 2, 2], [4, 1, 3]]
 
+        make a heap = [(2, 0) ]
+                         ^
         '''
 
-        # push original index
-        for index, t in enumerate(tasks):
-            t.append(index)
+        # tasks = [[1, 2, 0], [2, 4, 1], [3, 2, 2], [4, 1, 3]]
+        for i, t in enumerate(tasks):
+            t.append(i)
         
         # sort by enqueue time
         tasks.sort(key=lambda x:x[0])
-
-        # (process_time, index)
-        heap: list[tuple[int, int]] = []
-
+        
+        # make a heap = [(2, 0) ]
+        #                  ^
         i = 0
+        heap = []
         ret = []
-        time = 0
+        time = tasks[0][0]
 
         while len(ret) < len(tasks):
-            while i <= len(tasks)-1:
-                if tasks[i][0] <= time:
-                    heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
-                    i += 1
-                else:
-                    break
+            # pop from tasks, push to the heap
+            while i <= len(tasks)-1 and tasks[i][0] <= time:
+                # push (process_time, index)
+                heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
+                i += 1
             
             if not heap:
                 time = tasks[i][0]
-                continue
+            else:
+                process_time, index = heapq.heappop(heap)
 
-            process_time, org_index = heapq.heappop(heap)
-            ret.append(org_index)
+                ret.append(index)
 
-            time += process_time
-            
+                time += process_time
+        
+
         return ret
         
