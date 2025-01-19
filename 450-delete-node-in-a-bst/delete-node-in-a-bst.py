dec0451c05fce7664027f17a8a7b37ed43db1c68
Wonhee Jang <Qtsky89@gmail.com>
2025-01-19 16:51:53 +0900

    Time: 0 ms (100.00%) | Memory: 21.2 MB (57.23%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -4,44 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def smallest_node(self, node: TreeNode) -> TreeNode:
-        while node.left:
-            node = node.left
+    def biggestNode(self, node: TreeNode) -> TreeNode:
+        while node.right:
+            node = node.right
         return node
 
+
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        '''
-        1. if couldn't find,      => return base one => done
-
-        2. find one, no childeren => just remove the target => 
-        3. find one, one children => remove and children should be that position
-        4. find one, two children => remove and one of children should be that position (prefer left one)
-        '''
-
         if not root:
             return None
         
         if root.val == key:
-            # 2. find one, no childeren => just remove the target => 
+            # leaf
             if not root.left and not root.right:
                 return None
-            # 3. find one, one children => remove and children should be that position
             elif not root.left:
                 return root.right
             elif not root.right:
                 return root.left
-            # 4. find one, two children => remove and one of children should be that position (prefer right one)
-            else:
-                smallest_node = self.smallest_node(root.right)
-                root.val = smallest_node.val
-                root.right = self.deleteNode(root.right, smallest_node.val)
+            else: # two children is alive
+                node = self.biggestNode(root.left)
+                root.val = node.val
+                root.left = self.deleteNode(root.left, node.val)
+
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
         else:
             root.left = self.deleteNode(root.left, key)
-        
+
         return root
-
-
+        
