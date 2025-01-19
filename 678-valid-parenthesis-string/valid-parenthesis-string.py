e46c3d7e98493b7fc69e08cb4efd2668c098a760
Wonhee Jang <Qtsky89@gmail.com>
2025-01-19 21:28:16 +0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.31%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,37 +1,37 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         '''
-        Input: s = " ( * ) ) "
-                             ^
+        s = ( * ) )
+                ^
 
-        Output: true
+            ) * <= edgecase to deal with
 
-        left_stack = 
-        asterisk_stack =  
+        open_stack = [ 0 ]
+
+        asterisk_stack = [ 1 ]
         '''
 
-        left_stack = []
+        open_stack = []
         asterisk_stack = []
 
         for i in range(len(s)):
             if s[i] == '(':
-                left_stack.append(i)
+                open_stack.append(i)
             elif s[i] == '*':
                 asterisk_stack.append(i)
             elif s[i] == ')':
-                if (not left_stack) and (not asterisk_stack):
-                    return False
-                elif left_stack:
-                    left_stack.pop()
+                if open_stack:
+                    open_stack.pop()
                 elif asterisk_stack:
                     asterisk_stack.pop()
+                else:
+                    return False
+        
+        while open_stack and asterisk_stack:
+            i = open_stack.pop()
+            j = asterisk_stack.pop()
 
-        while left_stack and asterisk_stack:
-            tmp_left = left_stack.pop()
-            tmp_asterisk = asterisk_stack.pop()
-
-            # * (
-            if tmp_asterisk < tmp_left:
+            if j < i:
                 return False
-
-        return not left_stack
+        
+        return not open_stack
