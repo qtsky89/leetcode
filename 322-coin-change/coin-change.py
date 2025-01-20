368e1a29d82b826c4f147c14bbf73ff29f1fc81e
Wonhee Jang <Qtsky89@gmail.com>
2025-01-20 22:13:49 +0900

    Time: 711 ms (80.00%) | Memory: 18.2 MB (46.82%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,48 +1,30 @@
 class Solution:
-    # def coinChange(self, coins: List[int], amount: int) -> int:
-    #     '''
-    #     count_map = {
-    #         5 : 1
-    #         2 : 1
-    #         1 : 1
-    #     }
-
-    #     '''
-
-    #     coins.sort(reverse=True)
-
-    #     count_map = {}
-
-    #     def dfs(current_sum: int, coin_count: int) -> None:
-    #         if current_sum > amount:
-    #             return
-
-    #         if current_sum in count_map and count_map[current_sum] <= coin_count:
-    #             return
-            
-    #         count_map[current_sum] = coin_count
-                
-    #         for coin in coins:
-    #             dfs(current_sum+coin, coin_count+1)
-
-    #     dfs(0, 0)
-
-    #     return count_map[amount] if amount in count_map else -1
-    
-    
     def coinChange(self, coins: List[int], amount: int) -> int:
         '''
-        dp[11] = dp[6] + 1
-        dp[6] = dp[5] + 1
+        coin = 1
+
+        dp[1] = dp[0] + 1
+        dp[2] = dp[1] + 1
+        ...
+
+
+        coin = 5
         dp[5] = 1
+        dp[6] = 2
+        ...
+
+
+
+
+
+
         '''
-
-        dp = [float('inf')] * (amount + 1)
+    
+        dp = [float('inf')] * (amount+1)
         dp[0] = 0
-
         for coin in coins:
-            for i in range(coin, amount+1):
-                dp[i] = min(dp[i], dp[i-coin] + 1)
+            for i in range(coin, len(dp)):
+                dp[i] = min(dp[i], dp[i-coin]+1)
         
+
         return dp[-1] if dp[-1] != float('inf') else -1
-        
