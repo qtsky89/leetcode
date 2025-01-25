7de1c86152a5807f20e0cf2430219f5ada734886
Wonhee Jang <Qtsky89@gmail.com>
2025-01-25 19:28:14 +0900

    Time: 35 ms (37.06%) | Memory: 23.1 MB (29.27%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        '''
+        stones = [2, 7, 4, 1, 8, 1]
+
+        23 => 12
+        
+
+        12
+
+        23 - 24
+
+        
+
+        '''
+
+        sum_stones = sum(stones)
+        target = math.ceil(sum_stones / 2)
+
+        dp = {}
+        def dfs(i: int, current_sum: int) -> int:
+            if (i, current_sum) not in dp:
+                if current_sum >= target or i == len(stones):
+                    return abs(sum_stones - 2 * current_sum)
+                
+                dp[(i, current_sum)] = min(dfs(i+1, current_sum + stones[i]), dfs(i+1, current_sum))
+            
+            return dp[(i, current_sum)]
+
+        
+        return dfs(0, 0)
