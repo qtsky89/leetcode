14f78979e8d41da9004f860c5baf8cff32f840e7
Wonhee Jang <Qtsky89@gmail.com>
2025-09-07 17:07:44 +0900

    Time: 476 ms (97.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+# step1. make sub query to check how many empoyee_id : department_id count is
+WITH Department AS (SELECT employee_id, COUNT(*) AS department_count FROM Employee
+GROUP BY employee_id)
+
+# step2. Used where condition
+SELECT e.employee_id, e.department_id FROM Employee e
+INNER JOIN Department d ON e.employee_id = d.employee_id
+WHERE d.department_count = 1 OR e.primary_flag = 'Y'
