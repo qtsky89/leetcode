591d899f930fcac2218d200a6f965fdea3c446b6
Wonhee Jang <Qtsky89@gmail.com>
2025-09-21 23:33:33 +0900

    Time: 199 ms (44.46%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -5,5 +5,5 @@ WITH distinct_customer AS (SELECT DISTINCT visited_on FROM Customer)
 SELECT c1.visited_on, SUM(c2.amount) AS amount, ROUND(SUM(c2.amount)::numeric / 7, 2) AS average_amount FROM distinct_customer c1
 INNER JOIN Customer c2 ON (c1.visited_on - c2.visited_on) >= 0 AND (c1.visited_on - c2.visited_on) <= 6
 GROUP BY c1.visited_on
-HAVING COUNT(DISTINCT c2.visited_on) >= 7
+HAVING COUNT(DISTINCT c2.visited_on) = 7
 ORDER BY c1.visited_on ASC
