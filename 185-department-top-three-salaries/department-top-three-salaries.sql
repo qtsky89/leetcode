beeaf2697cdcf4656fcacb3153964176e65b85ec
Wonhee Jang <Qtsky89@gmail.com>
2025-10-05 21:34:57 +0900

    Time: 299 ms (34.97%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,11 +1,21 @@
 -- Write your PostgreSQL query statement below
 
-SELECT d.name AS "Department", e.name AS "Employee", e.salary AS "Salary" FROM (SELECT 
-    departmentId, 
-    name, 
-    salary,
-    DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rank
-FROM Employee) e
-INNER JOIN Department d ON e.departmentId = d.id
-WHERE e.rank <= 3
-ORDER BY e.rank DESC
+-- SELECT d.name AS "Department", e.name AS "Employee", e.salary AS "Salary" FROM (SELECT 
+--     departmentId, 
+--     name, 
+--     salary,
+--     DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rank
+-- FROM Employee) e
+-- INNER JOIN Department d ON e.departmentId = d.id
+-- WHERE e.rank <= 3
+-- ORDER BY e.rank DESC
+
+
+SELECT d.name AS "Department", e.name AS "Employee", e.salary AS "Salary"
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+) <= 3
