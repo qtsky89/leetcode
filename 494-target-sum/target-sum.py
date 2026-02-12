f83353afdea9a3e2dc6b86c1cf80da027d2e6997
Wonhee Jang <Qtsky89@gmail.com>
2026-02-12 23:30:18 +0900

    Time: 129 ms (55.58%) | Memory: 19.6 MB (54.86%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,18 +1,33 @@
 class Solution:
     # dfs with cache memoization
+    # def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+    #     cache = {}
+
+    #     def dfs(i: int, current_total: int) -> int:
+    #         if i == len(nums):
+    #             if current_total == target:
+    #                 return 1
+    #             else:
+    #                 return 0
+
+    #         if (i, current_total) not in cache:           
+    #             cache[(i, current_total)] = dfs(i+1, current_total + nums[i]) + dfs(i+1, current_total - nums[i])
+    #         return cache[(i, current_total)]
+        
+    #     return dfs(0, 0)
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        
-        cache = {}
+        dp = [defaultdict(int) for _ in range(len(nums)+1)]
 
-        def dfs(i: int, current_total: int) -> int:
-            if i == len(nums):
-                if current_total == target:
-                    return 1
-                else:
-                    return 0
-
-            if (i, current_total) not in cache:           
-                cache[(i, current_total)] = dfs(i+1, current_total + nums[i]) + dfs(i+1, current_total - nums[i])
-            return cache[(i, current_total)]
+        dp[0][0] = 1 # (e element, currnt sum is 0) => 1 way
         
-        return dfs(0, 0)
+        for i in range(len(nums)):
+            for cur_sum, count in dp[i].items():
+                dp[i+1][cur_sum + nums[i]] += count
+                dp[i+1][cur_sum - nums[i]] += count
+        
+
+        return dp[-1][target]
+
+
+        
